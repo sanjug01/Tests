0aaa48ad1808b270f60252e218df02e0fd39fc93
Diman Todorov <diman.todorov@outlook.com>
2014-10-14 13:02:30 -0700

    RdClientCx binary drop (release versions broken!)

---
+++ RdClientCx/scripts/build_rdclientcx.ps1
@@ -0,0 +1,28 @@
+$SDXROOT = "c:\sdx\clouddv"
+
+Function BcxPlatform($platform)
+{
+	$pinfo = New-Object System.Diagnostics.ProcessStartInfo
+	$pinfo.FileName = "cmd.exe"
+	$pinfo.RedirectStandardInput = $true
+	$pinfo.RedirectStandardError = $false
+	$pinfo.RedirectStandardOutput = $false
+	$pinfo.UseShellExecute = $false
+	$p = New-Object System.Diagnostics.Process
+	$p.StartInfo = $pinfo
+	$p.Start()
+	$p.StandardInput.WriteLine("cd $SDXROOT")
+	$p.StandardInput.WriteLine(".\tools\razzle.cmd $platform no_certcheck no_oacr no_opt")
+	$p.StandardInput.WriteLine("cd .\termsrv\rdp\winrt\RdClientCx\")
+	$p.StandardInput.WriteLine("bcx.cmd")
+	$p.StandardInput.WriteLine("exit")
+	$p.WaitForExit()
+}
+
+BcxPlatform "x86chk"
+BcxPlatform "x86fre"
+BcxPlatform "armchk"
+BcxPlatform "armfre"
+
+xcopy $SDXROOT\testsrc\termsrvtestdata\REDIST\WinRTClientBin\Debug ..\WinRTClientBin\Debug /i /s /y
+xcopy $SDXROOT\testsrc\termsrvtestdata\REDIST\WinRTClientBin\Release ..\WinRTClientBin\Release /i /s /y
