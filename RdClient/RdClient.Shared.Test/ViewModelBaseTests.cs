d87ad9a94a5111ea737ff09603c8e01ef5273de3
Diman Todorov <diman.todorov@outlook.com>
2014-10-16 16:51:53 -0700

    changed test to use lambdas

--- RdClient/RdClient.Shared.Test/ViewModelBaseTests.cs
+++ RdClient/RdClient.Shared.Test/ViewModelBaseTests.cs
@@ -12,22 +12,10 @@ namespace Test.RdClient.Shared.Test
         { 
             private bool _someProperty = false;
 
-            public bool ChangedEventFired = false;
-            public PropertyChangedEventArgs ChangedEventArgs;
-
             public bool SomeProperty
             {
                 get { return _someProperty; }
                 set { SetProperty(ref _someProperty, value, "SomeProperty"); }
-            }
-
-            public static void c_SomePropertyChanged(object sender, EventArgs e)
-            {
-                DummyViewModel dvm = (DummyViewModel) sender;
-                PropertyChangedEventArgs args = (PropertyChangedEventArgs)e;
-
-                dvm.ChangedEventFired = true;
-                dvm.ChangedEventArgs = args;
             }
         }
 
@@ -35,13 +23,13 @@ namespace Test.RdClient.Shared.Test
         public void ViewModelBaseTest_Success()
         {
             DummyViewModel dvm = new DummyViewModel();
+            string changedProperty = null;
 
-            dvm.PropertyChanged += DummyViewModel.c_SomePropertyChanged;
+            dvm.PropertyChanged += (s, e) => changedProperty = e.PropertyName;
 
             dvm.SomeProperty = true;
 
-            Assert.IsTrue(dvm.ChangedEventFired);
-            Assert.AreEqual("SomeProperty", dvm.ChangedEventArgs.PropertyName);
+            Assert.AreEqual("SomeProperty", changedProperty);
             Assert.AreEqual(true, dvm.SomeProperty);
         }
 
@@ -49,13 +37,13 @@ namespace Test.RdClient.Shared.Test
         public void ViewModelBaseTest_WriteSameValue()
         {
             DummyViewModel dvm = new DummyViewModel();
+            string changedProperty = null;
 
-            dvm.PropertyChanged += DummyViewModel.c_SomePropertyChanged;
+
+            dvm.PropertyChanged += (s, e) => changedProperty = e.PropertyName;
 
             dvm.SomeProperty = false;
-
-            Assert.IsFalse(dvm.ChangedEventFired);
-            Assert.AreEqual(null, dvm.ChangedEventArgs);
+            Assert.AreEqual(null, changedProperty);
         }
     }
 }
