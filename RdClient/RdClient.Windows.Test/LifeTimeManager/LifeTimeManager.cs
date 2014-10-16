e28f63c01d55c9823163bae59d057d0571e50e4f
Diman Todorov <diman.todorov@outlook.com>
2014-10-15 17:19:16 -0700

    working on root view

---
+++ RdClient/RdClient.Windows.Test/LifeTimeManager/LifeTimeManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Windows.UI.Xaml;
+
+namespace RdClient.Windows.Test
+{
+    [TestClass]
+    public class LifeTimeManager
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
--- RdClient/RdClient.Windows/MainPage.xaml.cs
+++ RdClient/RdClient.Windows/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RdClient.Navigation;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,18 +14,31 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace RdClient
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage : Page, IViewPresenter
     {
+        private UIElement _currentElement;
+        private UIElement _newElement;
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        public void PresentView(IPresentableView view)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PushModalView(IPresentableView view)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DismissModalView(IPresentableView view)
+        {
+            throw new NotImplementedException();
         }
     }
 }
