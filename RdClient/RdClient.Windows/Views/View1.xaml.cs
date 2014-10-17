8e611051bf10c4f683d098f80178d1e8af40b41b
Diman Todorov <diman.todorov@outlook.com>
2014-10-17 13:17:02 -0700

    showing that the navigation service works

--- RdClient/RdClient.Windows/Views/View1.xaml.cs
+++ RdClient/RdClient.Windows/Views/View1.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using RdClient.Navigation;
+using System.Diagnostics.Contracts;
 using Windows.UI.Xaml.Controls;
 
 namespace RdClient.Views
 {
     public sealed partial class View1 : Page, IPresentableView
     {
+        private INavigationService _navigationService;
+
         public View1()
         {
             this.InitializeComponent();
@@ -17,12 +20,19 @@ namespace RdClient.Views
 
         public void Presenting(INavigationService navigationService, object activationParameter)
         {
-            
+            Contract.Requires(navigationService != null);
+
+            _navigationService = navigationService;
         }
 
         public void Dismissing()
         {
 
+        }
+
+        private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            _navigationService.NavigateToView("view2", null);
         }
     }
 }
--- RdClient/RdClient.Windows/Views/View2.xaml.cs
+++ RdClient/RdClient.Windows/Views/View2.xaml.cs
@@ -1,11 +1,14 @@
 ﻿using RdClient.Navigation;
 using System;
+using System.Diagnostics.Contracts;
 using Windows.UI.Xaml.Controls;
 
 namespace RdClient.Views
 {
     public sealed partial class View2 : Page, IPresentableView
     {
+        private INavigationService _navigationService;
+
         public View2()
         {
             this.InitializeComponent();
@@ -18,12 +21,19 @@ namespace RdClient.Views
 
         public void Presenting(INavigationService navigationService, object activationParameter)
         {
-            
+            Contract.Requires(navigationService != null);
+
+            _navigationService = navigationService;
         }
 
         public void Dismissing()
         {
             
+        }
+
+        private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            _navigationService.NavigateToView("view1", null);
         }
     }
 }
