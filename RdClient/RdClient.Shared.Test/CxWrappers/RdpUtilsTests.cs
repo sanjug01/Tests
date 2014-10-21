01a54e6dcf8720bf6eb5857daea45872ad5660e5
Diman Todorov <diman.todorov@outlook.com>
2014-10-20 18:19:11 -0700

    added rudimentary test for rdp properties applier

---
+++ RdClient/RdClient.Shared.Test/CxWrappers/RdpUtilsTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RdClient.Shared.Models;
+using RdClient.Shared.CxWrappers;
+
+namespace RdClient.Shared.Test.CxWrappers
+{
+    [TestClass]
+    public class RdpUtilsTests
+    {
+        [TestMethod]
+        public void TestApplyRdpProperties()
+        {
+            Desktop desktop = new Desktop() { hostName = "narf" };
+            Mock.RdpProperties properties = new Mock.RdpProperties();
+
+            RdpUtils.ApplyRdpProperties(properties, desktop);
+
+            int hostNameIdx = properties._setStringPropertyName.IndexOf("Full Address");
+            Assert.IsTrue(hostNameIdx >= 0);
+            Assert.AreEqual("narf", properties._setStringPropertyValue[hostNameIdx]);
+        }
+    }
+}
