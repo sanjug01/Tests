3b968853db7164036196e520b3c0c04b8ee8b7f5
Diman Todorov <diman.todorov@outlook.com>
2014-10-24 12:11:19 -0700

    added a unit test

--- RdClient/RdClient.Shared.Test/CxWrappers/RdpUtilsTests.cs
+++ RdClient/RdClient.Shared.Test/CxWrappers/RdpUtilsTests.cs
@@ -10,7 +10,7 @@ namespace RdClient.Shared.Test.CxWrappers
     public class RdpUtilsTests
     {
         [TestMethod]
-        public void TestApplyRdpProperties()
+        public void TestApplyDesktop()
         {
             Desktop desktop = new Desktop() { hostName = "narf" };
             Mock.RdpProperties properties = new Mock.RdpProperties();
@@ -20,6 +20,23 @@ namespace RdClient.Shared.Test.CxWrappers
             int hostNameIdx = properties._setStringPropertyName.IndexOf("Full Address");
             Assert.IsTrue(hostNameIdx >= 0);
             Assert.AreEqual("narf", properties._setStringPropertyValue[hostNameIdx]);
+        }
+
+        public void TestApplyScreenSize()
+        {
+            Tuple<int, int> size = new Tuple<int, int>(23, 42);
+            Mock.RdpProperties properties = new Mock.RdpProperties();
+            Mock.PhysicalScreenSize physicalSize = new Mock.PhysicalScreenSize(size);
+
+            RdpPropertyApplier.ApplyScreenSize(properties, physicalSize);
+
+            int widthIdx = properties._setStringPropertyName.IndexOf("PhysicalDesktopWidth");
+            Assert.IsTrue(widthIdx >= 0);
+            Assert.AreEqual(23, properties._setStringPropertyValue[widthIdx]);
+
+            int heightIdx = properties._setStringPropertyName.IndexOf("PhysicalDesktopHeight");
+            Assert.IsTrue(heightIdx >= 0);
+            Assert.AreEqual(42, properties._setStringPropertyValue[heightIdx]);
         }
     }
 }
---
+++ RdClient/RdClient.Shared.Test/Mock/PhysicalScreenSize.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RdClient.Shared.Test.Mock
+{
+    public class PhysicalScreenSize : IPhysicalScreenSize
+    {
+        private Tuple<int, int> _size;
+        public PhysicalScreenSize(Tuple<int, int> size)
+        {
+            _size = size;
+        }
+
+        public Tuple<int, int> GetScreenSize()
+        {
+            return _size;
+        }
+    }
+}
