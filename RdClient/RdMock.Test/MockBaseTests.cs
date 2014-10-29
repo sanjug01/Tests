8b27041485128d7eb765aaf5e4b08d6407f5a003
Diman Todorov <diman.todorov@outlook.com>
2014-10-28 19:10:15 -0700

    addressed CR

--- RdClient/RdMock.Test/MockBaseTests.cs
+++ RdClient/RdMock.Test/MockBaseTests.cs
@@ -9,10 +9,19 @@ namespace RdMock.Test
     {
         class TestMock : MockBase
         {
-
-            public int testMethod(int a)
+            public int testMethod1(int a)
             {
                 return (int) Invoke(new object[] {a});
+            }
+
+            public int testMethod2(int a, char b)
+            {
+                return (int)Invoke(new object[] { a, b });
+            }
+
+            public int testMethod3(int a)
+            {
+                return (int)Invoke(new object[] { a });
             }
         }
 
@@ -26,12 +35,42 @@ namespace RdMock.Test
                     TestMock tm = new TestMock();
                     List<object> pars = new List<object>() { 3 };
 
-                    tm.Expect("testMethod", pars, 4);
-                    int actual = tm.testMethod(3);
+                    tm.Expect("testMethod1", pars, 4);
+                    int actual = tm.testMethod1(3);
                     Assert.AreEqual(4, actual);
                 }
             }
             catch(MockException /* e */)
+            {
+                exceptionThrown = true;
+            }
+
+            Assert.IsFalse(exceptionThrown);
+        }
+
+        [TestMethod]
+        public void TestMethodSequence()
+        {
+            bool exceptionThrown = false;
+            try
+            {
+                {
+                    TestMock tm = new TestMock();
+
+                    tm.Expect("testMethod1", new List<object>() { 3 }, 4)
+                      .Expect("testMethod2", new List<object>() { 3, 'a' }, 5)
+                      .Expect("testMethod3", new List<object>() { 23 }, 42);
+
+                    int actual1 = tm.testMethod1(3);
+                    int actual2 = tm.testMethod2(3, 'a');
+                    int actual3 = tm.testMethod3(23);
+
+                    Assert.AreEqual(4, actual1);
+                    Assert.AreEqual(5, actual2);
+                    Assert.AreEqual(42, actual3);
+                }
+            }
+            catch (MockException /* e */)
             {
                 exceptionThrown = true;
             }
--- RdClient/RdMock/MockBase.cs
+++ RdClient/RdMock/MockBase.cs
@@ -3,7 +3,49 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
-using System.Text;
+
+// The MockBase class is a basic mocking framework which reduces the amount of typing required to implement mock classes.
+//
+// A mock class is a class which pretends to be a full implementation but instead just returns the values it is setup for.
+//
+// For example, let's say we have a class Foo. The method narf() in the class Bar interacts with methods in Foo.
+// If we want to write a unit test for narf, we need to verify that the methods of the Foo instance contained in Bar have been called
+// in the correct sequence with the correct parameters and returned the expected values.
+//
+// To implement a Mock class for Foo which verifies these things we:
+//
+// class MockFoo : IBar, MockBase
+// {
+//      public int method(int param1, char param2)
+//      {
+//          return (int) Invoke(new object[] { param1, param2 };
+//      }
+// }
+// 
+//
+// We are assuming here that IBar only has one method with the signature int method(int param1, char param2).
+// Invoke is called on MockBase which through reflection gives information to the MockBase instance that the method has been called with param1 and param2
+// as parameters.
+//
+// Note: make sure you add all parameters in the same order as in the signature to the array passed to Invoke.
+//
+// To use the class we would do something along the lines of:
+// MockFoo mockFoo = new MockFoo();
+// Bar bar = new Bar(mockFoo);
+//
+// mockFoo.Expect("method", new object[] { 3, 4 }, 5);
+//
+// bar.methodUnderTest();
+//
+//
+// If methodUnderTest does anything with foo that's not rigged with an expect call, an exception is thrown and the test fails.
+//
+// This could be:
+//  * methodUnderTest invokes a method on foo which we are not expecting
+//  * methodUnderTest doesn't invoke all methods we expect it to invoke
+//  * methodUnderTest invoked the method when we wanted it to but with parameters we didn't expect
+//
+// After we have verified that the method is indeed an expected invocation, the last parameter of Expect (here it is 5) is returned as a return value of foo.method().
 
 namespace RdMock
 {
@@ -29,17 +71,14 @@ namespace RdMock
 
     public class MockBase
     {
-        private IDictionary<string, IList<MockCall>> _calls = new Dictionary<string, IList<MockCall>>();
-        private IDictionary<string, IList<MockReturn>> _returns = new Dictionary<string, IList<MockReturn>>();
+        private IList<MockCall> _calls = new List<MockCall>();
+        private IList<MockReturn> _returns = new List<MockReturn>();
 
         ~MockBase()
         {
-            foreach (string functionName in _calls.Keys)
+            if(_calls.Count > 0)
             {
-                if (_calls[functionName].Count > 0)
-                {
-                    throw new MockException("Expecing " + functionName + " to be called " + _calls[functionName].Count + " more times.");
-                }
+                throw new MockException("Expected " + _calls.Count + " more calls before exiting scope.");
             }
         }
 
@@ -48,19 +87,8 @@ namespace RdMock
             MockCall call = new MockCall() { functionName = functionName_, parameters = parameters_ };
             MockReturn retval = new MockReturn() { functionName = functionName_, value = value_ };
 
-            if (_calls.ContainsKey(functionName_) == false)
-            {
-                _calls[functionName_] = new List<MockCall>();
-            }
-
-            _calls[functionName_].Add(call);
-
-            if (_returns.ContainsKey(functionName_) == false)
-            {
-                _returns[functionName_] = new List<MockReturn>();
-            }
-
-            _returns[functionName_].Add(retval);
+            _calls.Add(call);
+            _returns.Add(retval);
 
             return this;
         }
@@ -81,13 +109,13 @@ namespace RdMock
 
             string methodName = method.Name;
 
-            if(_calls.ContainsKey(methodName) == false || _calls[methodName].Count() < 1)
+            if(_calls[0].functionName.Equals(methodName) == false || _calls.Count < 1)
             {
                 throw new MockException("Unexpected invocation of " + methodName);
             }
 
-            MockCall call = _calls[methodName][0];
-            _calls[methodName].RemoveAt(0);
+            MockCall call = _calls[0];
+            _calls.RemoveAt(0);
 
             IList<object> expectedParameters = call.parameters;
             ParameterInfo[] actualParameters = method.GetParameters();
@@ -116,13 +144,13 @@ namespace RdMock
                 }
             }
 
-            if(_returns.ContainsKey(methodName) == false || _returns[methodName].Count < 1)
+            if(_returns[0].functionName.Equals(methodName) == false || _returns.Count < 1)
             {
                 throw new MockException("Initialized expectation but missing return value?!");
             }
 
-            object retval = _returns[methodName][0].value;
-            _returns[methodName].RemoveAt(0);
+            object retval = _returns[0].value;
+            _returns.RemoveAt(0);
 
             return retval;
         }
