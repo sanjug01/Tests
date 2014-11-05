6f161c08b7cd0837b7ee177e82f228d385ef07fb
Alexandru Jugravu <Alexandru.Jugravu@microsoft.com>
2014-11-04 16:40:46 -0800

    Add initial view and view model for add/edit desktop

---
+++ RdClient/RdClient.Shared/ViewModels/IAddOrEditDesktopViewModel.cs
@@ -0,0 +1,14 @@
+﻿using RdClient.Navigation;
+using RdClient.Shared.CxWrappers;
+using System;
+using System.Windows.Input;
+
+namespace RdClient.Shared.ViewModels
+{
+    public interface IAddOrEditDesktopViewModel
+    {
+        ICommand SaveCommand { get; }
+        ICommand CancelCommand { get; }
+        INavigationService NavigationService { set; }
+    }
+}
