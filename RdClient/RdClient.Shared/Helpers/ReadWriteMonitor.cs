43bb61d5a00a83a97b1fa2d2d6323cd76de4a2db
Pavel Karpenko <pakarpen@microsoft.com>
2014-11-06 19:17:59 -0800

    Added 2 helper classes for future work on the app bar. Refactored the RelayCommand class to subscribe for and emit events in the thread-safe fashion.

---
+++ RdClient/RdClient.Shared/Helpers/ReadWriteMonitor.cs
@@ -0,0 +1,144 @@
+﻿namespace RdClient.Shared.Helpers
+{
+    using System;
+    using System.Diagnostics.Contracts;
+    using System.Threading;
+
+    /// <summary>
+    /// Factory of disposable read-write monitors designed to be used with the "using" C# statement.
+    /// The class guarantees that acquisition of the monitor will be matched with its release.
+    /// </summary>
+    public static class ReadWriteMonitor
+    {
+        /// <summary>
+        /// Acquire the lock object for reading and create a disposable object that will release the lock
+        /// upon disposing.
+        /// </summary>
+        /// <param name="rwLock">Reader-writer lock to aquireand release.</param>
+        /// <returns>Disposable object that releases the lock upon disposing</returns>
+        public static IDisposable Read(ReaderWriterLockSlim rwLock)
+        {
+            Contract.Requires(null != rwLock);
+            Contract.Ensures(null != Contract.Result<IDisposable>());
+            return ReadLock.Create(rwLock);
+        }
+
+        /// <summary>
+        /// Acquire the lock object for upgradeable reading and create a disposable object that will release the lock
+        /// upon disposing.
+        /// </summary>
+        /// <param name="rwLock">Reader-writer lock to aquireand release.</param>
+        /// <returns>Disposable object that releases the lock upon disposing</returns>
+        public static IDisposable UpgradeableRead(ReaderWriterLockSlim rwLock)
+        {
+            Contract.Requires(null != rwLock);
+            Contract.Ensures(null != Contract.Result<IDisposable>());
+            return UpgradeableReadLock.Create(rwLock);
+        }
+
+        /// <summary>
+        /// Acquire the lock object for writing and create a disposable object that will release the lock
+        /// upon disposing.
+        /// </summary>
+        /// <param name="rwLock">Reader-writer lock to aquireand release.</param>
+        /// <returns>Disposable object that releases the lock upon disposing</returns>
+        public static IDisposable Write(ReaderWriterLockSlim rwLock)
+        {
+            Contract.Requires(null != rwLock);
+            Contract.Ensures(null != Contract.Result<IDisposable>());
+            return WriteLock.Create(rwLock);
+        }
+
+        private abstract class LockBase : DisposableObject
+        {
+            protected readonly ReaderWriterLockSlim _rwLock;
+
+            protected abstract IDisposable Acquire();
+            protected abstract void Release();
+
+            protected LockBase(ReaderWriterLockSlim rwLock)
+            {
+                Contract.Requires(null != rwLock);
+                Contract.Ensures(null != _rwLock);
+                _rwLock = rwLock;
+            }
+
+            protected override void DisposeManagedState()
+            {
+                Release();
+            }
+        }
+
+        private sealed class ReadLock : LockBase
+        {
+            public static IDisposable Create(ReaderWriterLockSlim rwLock)
+            {
+                Contract.Requires(null != rwLock);
+                Contract.Ensures(null != Contract.Result<IDisposable>());
+                return (new ReadLock(rwLock)).Acquire();
+            }
+
+            private ReadLock(ReaderWriterLockSlim rwLock) : base(rwLock) { }
+
+            protected override IDisposable Acquire()
+            {
+                Contract.Ensures(null != Contract.Result<IDisposable>());
+                _rwLock.EnterReadLock();
+                return this;
+            }
+
+            protected override void Release()
+            {
+                _rwLock.ExitReadLock();
+            }
+        }
+
+        private sealed class UpgradeableReadLock : LockBase
+        {
+            public static IDisposable Create(ReaderWriterLockSlim rwLock)
+            {
+                Contract.Requires(null != rwLock);
+                Contract.Ensures(null != Contract.Result<IDisposable>());
+                return (new UpgradeableReadLock(rwLock)).Acquire();
+            }
+
+            private UpgradeableReadLock(ReaderWriterLockSlim rwLock) : base(rwLock) { }
+
+            protected override IDisposable Acquire()
+            {
+                Contract.Ensures(null != Contract.Result<IDisposable>());
+                _rwLock.EnterUpgradeableReadLock();
+                return this;
+            }
+
+            protected override void Release()
+            {
+                _rwLock.ExitUpgradeableReadLock();
+            }
+        }
+
+        private sealed class WriteLock : LockBase
+        {
+            public static IDisposable Create(ReaderWriterLockSlim rwLock)
+            {
+                Contract.Requires(null != rwLock);
+                Contract.Ensures(null != Contract.Result<IDisposable>());
+                return (new WriteLock(rwLock)).Acquire();
+            }
+
+            private WriteLock(ReaderWriterLockSlim rwLock) : base(rwLock) { }
+
+            protected override IDisposable Acquire()
+            {
+                Contract.Ensures(null != Contract.Result<IDisposable>());
+                _rwLock.EnterWriteLock();
+                return this;
+            }
+
+            protected override void Release()
+            {
+                _rwLock.ExitWriteLock();
+            }
+        }
+    }
+}
--- RdClient/RdClient.Shared/ViewModels/RelayCommand.cs
+++ RdClient/RdClient.Shared/ViewModels/RelayCommand.cs
@@ -1,26 +1,31 @@
 ﻿using System;
 using System.Diagnostics.Contracts;
+using System.Threading;
 using System.Windows.Input;
 
 namespace RdClient.Shared.ViewModels
 {
-    // This is a helper class which gives us a simple, generic implementation
-    // of ICommand. Its purpose is to feed it to XAML bindings so we can
-    // bind to actions.
-    public class RelayCommand : ICommand
+    /// <summary>
+    /// This is a helper class which gives us a simple, generic implementation
+    /// of ICommand. Its purpose is to feed it to XAML bindings so we can
+    /// bind to actions.
+    /// </summary>
+    /// <remarks>The class is disposable because it has a disposable member - a reader-writer
+    /// monitor used to make subscribing and emitting of the event thread-safe.</remarks>
+    public class RelayCommand : Helpers.DisposableObject, ICommand
     {
-        readonly Action<object> _execute;
-        readonly Predicate<object> _canExecute;
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+        private readonly ReaderWriterLockSlim _monitor;
+        private EventHandler _canExecuteChanged;
 
-        public RelayCommand(Action<object> execute)
-            : this(execute, null)
-        {
-        }
-
-        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
         {
             Contract.Requires(execute != null);
+            Contract.Ensures(null != _monitor);
+            Contract.Ensures(null != _execute);
 
+            _monitor = new ReaderWriterLockSlim(LockRecursionPolicy.SupportsRecursion);
             _execute = execute;
             _canExecute = canExecute;
         }
@@ -35,14 +40,40 @@ namespace RdClient.Shared.ViewModels
             _execute(parameter);
         }
 
-        public event EventHandler CanExecuteChanged;
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                ThrowIfDisposed();
+                using (Helpers.ReadWriteMonitor.Write(_monitor))
+                    _canExecuteChanged += value;
+            }
+
+            remove
+            {
+                ThrowIfDisposed();
+                using (Helpers.ReadWriteMonitor.Write(_monitor))
+                    _canExecuteChanged -= value;
+            }
+        }
 
         public void EmitCanExecuteChanged(object sender, EventArgs args)
         {
-            if(CanExecuteChanged != null)
+            EventHandler handler;
+
+            ThrowIfDisposed();
+            using (Helpers.ReadWriteMonitor.Read(_monitor))
+                handler = _canExecuteChanged;
+
+            if(null != handler)
             {
-                CanExecuteChanged(sender, args);
+                handler(sender, args);
             }
+        }
+
+        protected override void DisposeManagedState()
+        {
+            _monitor.Dispose();
         }
     }
 }
