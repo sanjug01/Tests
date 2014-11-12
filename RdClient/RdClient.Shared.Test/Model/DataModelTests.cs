3842a36a4b07ac90982d024a1b4dbf080468acd0
James Dudley <jamesdu@microsoft.com>
2014-11-12 10:26:34 -0800

    Finish data model unit tests

--- RdClient/RdClient.Shared.Test/Model/DataModelTests.cs
+++ RdClient/RdClient.Shared.Test/Model/DataModelTests.cs
@@ -3,96 +3,188 @@ using System;
 
 using RdClient.Shared.Models;
 using RdClient.Shared.Test.Mock;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace RdClient.Shared.Test.Model
 {
-    [TestClass]
+    class TestData
+    {
+        private Random _rand = new Random();
+
+        public Random RandomSource
+        { 
+            get
+            {
+                return _rand;
+            }
+        }
+
+        public string NewRandomString()
+        {
+            return "rand" + RandomSource.Next();
+        }
+
+        public Desktop NewValidDesktop()
+        {
+            return new Desktop() { HostName = NewRandomString(), CredentialId = Guid.NewGuid() };
+        }
+
+        public List<Desktop> NewSmallListOfDesktops()
+        {
+            int count = RandomSource.Next(3, 10);
+            List<Desktop> desktops = new List<Desktop>(count);
+            for (int i = 0; i < count; i++)
+            {
+                desktops.Add(NewValidDesktop());
+            }
+            return desktops;
+        }
+
+        public Credentials NewValidCredential()
+        {
+            return new Credentials()
+            {
+                Domain = NewRandomString(),
+                Username = NewRandomString(),
+                Password = NewRandomString()
+            };
+        }
+
+        public List<Credentials> NewSmallListOfCredentials()
+        {
+            int count = RandomSource.Next(3, 10);
+            List<Credentials> creds = new List<Credentials>(count);
+            for (int i = 0; i < count; i++)
+            {
+                creds.Add(NewValidCredential());
+            }
+            return creds;            
+        }
+    }
+    
     public abstract class DataModelTests
     {
+        private TestData _testData;
+        private Mock.DataStorage _mockStorage;
+        private IDataModel _dataModel;
+        private List<Desktop> _expectedDesktops;
+        private ObservableCollection<Desktop> _actualDesktops;
+        private ObservableCollection<Credentials> _actualCredentials;
+        private List<Credentials> _expectedCreds;
+
         protected abstract IDataModel GetDataModel(IDataStorage storage);
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            _testData = new TestData();
+            Mock.DataStorage mockStorage = new Mock.DataStorage();
+            _expectedDesktops = _testData.NewSmallListOfDesktops();
+            _expectedCreds = _testData.NewSmallListOfCredentials();
+            mockStorage.Expect("LoadDesktops", new List<object>() { }, _expectedDesktops);
+            mockStorage.Expect("LoadCredentials", new List<object>() { }, _expectedCreds);
+            _dataModel = GetDataModel(mockStorage);
+            _actualDesktops = _dataModel.Desktops;
+            _actualCredentials = _dataModel.Credentials;
+        }
 
         [TestMethod]
         public void LoadsDataFromStorage()
         {
-            throw new NotImplementedException();
+            CollectionAssert.AreEqual(_expectedDesktops, _actualDesktops);
+            CollectionAssert.AreEqual(_expectedCreds, _actualCredentials);
         }
 
         [TestMethod]
         public void AddDesktopSavesItToStorage()
         {
-            throw new NotImplementedException();
-        }
-
-        [TestMethod]
-        public void AddDesktopFailsIfItIsAlreadyAdded()
-        {
-            throw new NotImplementedException();
+            Desktop desktop = _testData.NewValidDesktop();
+            _mockStorage.Expect("SaveDesktop", new List<object>(){desktop}, 0);
+            _dataModel.Desktops.Add(desktop);            
         }
 
         [TestMethod]
         public void EditDesktopSavesItToStorage()
         {
-            throw new NotImplementedException();
+            Desktop desktop = _actualDesktops[_testData.RandomSource.Next(0, _actualDesktops.Count)];
+            _mockStorage.Expect("SaveDesktop", new List<object>() { desktop }, 0);
+            desktop.HostName = _testData.NewRandomString();
         }
 
         [TestMethod]
         public void DeleteDesktopRemovesItFromStorage()
         {
-            throw new NotImplementedException();
+            Desktop desktop = _actualDesktops[_testData.RandomSource.Next(0, _actualDesktops.Count)];
+            _mockStorage.Expect("SaveDesktop", new List<object>() { desktop }, 0);
+            _actualDesktops.Remove(desktop);
         }
 
         [TestMethod]
         public void AddCredentialSavesItToStorage()
         {
-            throw new NotImplementedException();
-        }
-
-        [TestMethod]
-        public void AddCredentialFailsIfItIsAlreadyAdded()
-        {
-            throw new NotImplementedException();
+            Credentials cred = _testData.NewValidCredential();
+            _mockStorage.Expect("SaveCredential", new List<object>() { cred }, 0);
+            _dataModel.Credentials.Add(cred);
         }
 
         [TestMethod]
         public void EditCredentialSavesItToStorage()
         {
-            throw new NotImplementedException();
+            Credentials cred = _actualCredentials[_testData.RandomSource.Next(0, _actualCredentials.Count)];
+            _mockStorage.Expect("SaveCredential", new List<object>() { cred }, 0);
+            cred.Password = _testData.NewRandomString();           
         }
 
         [TestMethod]
         public void DeleteCredentialRemovesItsIdFromAllDesktops()
         {
-            throw new NotImplementedException();
+            Credentials cred = _actualCredentials[_testData.RandomSource.Next(0, _actualCredentials.Count)];
+            Desktop desktop = _actualDesktops[_testData.RandomSource.Next(0, _actualDesktops.Count)];
+            _mockStorage.Expect("SaveDesktop", new List<object>() { desktop }, 0);//change credId of desktop to match cred
+            desktop.CredentialId = cred.Id;
+
+            _mockStorage.Expect("SaveDesktop", new List<object>() { desktop }, 0);//remove credId of desktop as cred is removed
+            _mockStorage.Expect("DeleteCredential", new List<object>() { cred }, 0);            
+            _dataModel.Credentials.Remove(cred);
         }
 
         [TestMethod]
         public void DeleteCredentialRemovesItFromStorage()
         {
-            throw new NotImplementedException();
+            Credentials cred = _actualCredentials[_testData.RandomSource.Next(0, _actualCredentials.Count)];
+            _mockStorage.Expect("DeleteCredential", new List<object>() { cred }, 0);
+            _dataModel.Credentials.Remove(cred);
         }
 
         [TestMethod]
         public void GetDesktopWithIdReturnsAddedDesktop()
         {
-            throw new NotImplementedException();
+            Desktop expectedDesktop = _actualDesktops[_testData.RandomSource.Next(0, _actualDesktops.Count)];
+            Desktop actualDesktop = _dataModel.GetDesktopWithId(expectedDesktop.Id);
+            Assert.AreSame(expectedDesktop, actualDesktop);
         }
 
         [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
         public void GetDesktopWithIdThrowsExceptionIfIdNotAdded()
-        {
-            throw new NotImplementedException();
+        {            
+            _dataModel.GetDesktopWithId(Guid.NewGuid());
         }
 
         [TestMethod]
         public void GetCredentialWithIdReturnsAddedCredential()
         {
-            throw new NotImplementedException();
+            Credentials expectedCred = _actualCredentials[_testData.RandomSource.Next(0, _actualCredentials.Count)];
+            Credentials actualCred = _dataModel.GetCredentialWithId(expectedCred.Id);
+            Assert.AreSame(expectedCred, actualCred);
         }
 
         [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
         public void GetCredentialWithIdThrowsExceptionIfIdNotAdded()
         {
-            throw new NotImplementedException();
+            _dataModel.GetCredentialWithId(Guid.NewGuid());
         }
     }
 }
