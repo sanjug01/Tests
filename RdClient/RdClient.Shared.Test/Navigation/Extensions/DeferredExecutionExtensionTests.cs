70ab81d1848ed8af0d0de1ff4f3fc807bbe6587e
Pavel Karpenko <pakarpen@microsoft.com>
2014-11-20 15:38:20 -0800

    Add unit test for DeferredExecutionExtension

---
+++ RdClient/RdClient.Shared.Test/Navigation/Extensions/DeferredExecutionExtensionTests.cs
@@ -0,0 +1,110 @@
+﻿namespace RdClient.Shared.Test.Navigation.Extensions
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RdClient.Shared.Helpers;
+    using RdClient.Shared.Navigation;
+    using RdClient.Shared.Navigation.Extensions;
+    using System;
+    using System.Collections.Generic;
+
+    [TestClass]
+    public class DeferredExecutionExtensionTests
+    {
+        private sealed class TestDeferredExecution : IDeferredExecution
+        {
+            void IDeferredExecution.Defer(Action action)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        private sealed class TestViewModel : MutableObject, IViewModel, IDeferredExecutionSite
+        {
+            private IDeferredExecution _deferredExecution;
+
+            public IDeferredExecution DeferredExecution
+            {
+                get { return _deferredExecution; }
+                set { this.SetProperty<IDeferredExecution>(ref _deferredExecution, value); }
+            }
+
+            public void Presenting(INavigationService navigationService, object activationParameter)
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public void Dismissing()
+            {
+                throw new System.NotImplementedException();
+            }
+
+            void IDeferredExecutionSite.SetDeferredExecution(IDeferredExecution defEx)
+            {
+                this.DeferredExecution = defEx;
+            }
+        }
+
+        private TestDeferredExecution _defEx;
+        private DeferredExecutionExtension _extension;
+
+        [TestInitialize]
+        public void SetUpTest()
+        {
+            _defEx = new TestDeferredExecution();
+            _extension = new DeferredExecutionExtension() { DeferredExecution = _defEx };
+        }
+
+        [TestCleanup]
+        public void TearDownTest()
+        {
+            _extension = null;
+            _defEx = null;
+        }
+
+        [TestMethod]
+        public void NewDeferredExecutionExtension_CorrectDeferredExecution()
+        {
+            Assert.AreSame(_defEx, _extension.DeferredExecution);
+        }
+
+        [TestMethod]
+        public void ChangeDeferredExecution_ChangeReported()
+        {
+            IList<string> reportedProperties = new List<string>();
+
+            _extension.PropertyChanged += (s, e) => reportedProperties.Add(e.PropertyName);
+            _extension.DeferredExecution = null;
+            Assert.IsNull(_extension.DeferredExecution);
+            Assert.AreEqual(1, reportedProperties.Count);
+            Assert.AreEqual("DeferredExecution", reportedProperties[0]);
+        }
+
+        [TestMethod]
+        public void PresentViewModel_DeferredExecutionSet()
+        {
+            TestViewModel testVM = new TestViewModel();
+            IList<string> reportedProperties = new List<string>();
+
+            testVM.PropertyChanged += (s, e) => reportedProperties.Add(e.PropertyName);
+
+            _extension.CastAndCall<INavigationExtension>(ext => ext.Presenting(testVM));
+            Assert.AreSame(testVM.DeferredExecution, _defEx);
+            Assert.AreEqual(1, reportedProperties.Count);
+            Assert.AreEqual("DeferredExecution", reportedProperties[0]);
+        }
+
+        [TestMethod]
+        public void DismissViewModel_DeferredExecutionCleared()
+        {
+            TestViewModel testVM = new TestViewModel();
+            IList<string> reportedProperties = new List<string>();
+
+            _extension.CastAndCall<INavigationExtension>(ext => ext.Presenting(testVM));
+            testVM.PropertyChanged += (s, e) => reportedProperties.Add(e.PropertyName);
+            _extension.CastAndCall<INavigationExtension>(ext => ext.Dismissed(testVM));
+            Assert.IsNull(testVM.DeferredExecution);
+            Assert.AreEqual(1, reportedProperties.Count);
+            Assert.AreEqual("DeferredExecution", reportedProperties[0]);
+        }
+    }
+}
