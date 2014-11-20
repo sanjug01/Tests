e1ea5d9e82b0d2b6c428c4349f7bd19e205555c0
Pavel Karpenko <pakarpen@microsoft.com>
2014-11-20 14:26:37 -0800

    Implement the deferred execution navigation extension.

---
+++ RdClient/RdClient.Shared/Navigation/Extensions/DeferredExecutionExtension.cs
@@ -0,0 +1,30 @@
+﻿namespace RdClient.Shared.Navigation.Extensions
+{
+    using RdClient.Shared.Helpers;
+
+    /// <summary>
+    /// Navigation service extension that attaches an IDeferredExecution object to view models.
+    /// Having the deferred execution object, a view model can use it to defer its update until
+    /// an appropriate moment (most practical deferrals are posted to the UI thread).
+    /// </summary>
+    public sealed class DeferredExecutionExtension : MutableObject, INavigationExtension, IDeferredExecutionSite
+    {
+        private IDeferredExecution _deferredExecution;
+
+        void INavigationExtension.Presenting(IViewModel viewModel)
+        {
+            viewModel.CastAndCall<IDeferredExecutionSite>(site => site.DeferredExecution = _deferredExecution);
+        }
+
+        void INavigationExtension.Dismissed(IViewModel viewModel)
+        {
+            viewModel.CastAndCall<IDeferredExecutionSite>(site => site.DeferredExecution = null);
+        }
+
+        public IDeferredExecution DeferredExecution
+        {
+            get { return _deferredExecution; }
+            set { this.SetProperty<IDeferredExecution>(ref _deferredExecution, value); }
+        }
+    }
+}
---
+++ RdClient/RdClient.Shared/Navigation/Extensions/IDeferredExecutionSite.cs
@@ -0,0 +1,14 @@
+﻿namespace RdClient.Shared.Navigation.Extensions
+{
+    using RdClient.Shared.Helpers;
+
+    /// <summary>
+    /// Interface of a site of the IDeferredExecution interface.
+    /// The interface is used by the deferred execution extension (DeferredExecutionExtension class)
+    /// to attach and detach the IDeferredExecution object to and from the site.
+    /// </summary>
+    public interface IDeferredExecutionSite
+    {
+        IDeferredExecution DeferredExecution { get; set; }
+    }
+}
