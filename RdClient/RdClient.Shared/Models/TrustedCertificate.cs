0cedd4a030346648ae2c16dd266d43b8f4e32c16
Pavel Karpenko <pwkpwk@hotmail.com>
2014-12-11 12:44:04 -0800

    Restored a missing file

---
+++ RdClient/RdClient.Shared/Models/TrustedCertificate.cs
@@ -0,0 +1,56 @@
+namespace RdClient.Shared.Models
+{
+    using RdClient.Shared.CxWrappers;
+    using System;
+    using System.Diagnostics.Contracts;
+    using System.Runtime.Serialization;
+
+    [DataContract(IsReference = true)]
+    public class TrustedCertificate : ModelBase
+    {
+        private byte[] _hash;
+        private byte[] _serialNumber;
+
+        [DataMember]
+        public byte[] Hash
+        {
+            get { return _hash; }
+            set { this.SetProperty<byte[]>(ref _hash, value); }
+        }
+
+        [DataMember]
+        public byte[] SerialNumber
+        {
+            get { return _serialNumber; }
+            set { this.SetProperty<byte[]>(ref _serialNumber, value); }
+        }
+
+        public TrustedCertificate(IRdpCertificate certificate)
+        {
+            Contract.Requires(null != certificate);
+            Contract.Ensures(null != _hash);
+            Contract.Ensures(null != _serialNumber);
+
+            _hash = certificate.GetHashValue();
+            Contract.Assert(null != _hash);
+            _serialNumber = certificate.SerialNumber;
+            Contract.Assert(null != _serialNumber);
+        }
+
+        public TrustedCertificate()
+        {
+            //
+            // Default constructor required for serialization.
+            //
+        }
+
+        public bool IsMatchingCertificate(IRdpCertificate certificate)
+        {
+            Contract.Requires(null != certificate);
+            Contract.Assert(null != _hash);
+            Contract.Assert(null != _serialNumber);
+            return Array.Equals(certificate.SerialNumber, _serialNumber)
+                && Array.Equals(certificate.GetHashValue(), _hash);
+        }
+    }
+}
