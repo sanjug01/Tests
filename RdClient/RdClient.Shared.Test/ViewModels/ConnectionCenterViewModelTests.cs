c15976d287ffd93a77872eb899c9fb0a00bc4755
James Dudley <jamesdu@microsoft.com>
2014-12-10 17:28:10 -0800

    connection center and desktop view model tests

---
+++ RdClient/RdClient.Shared.Test/ViewModels/ConnectionCenterViewModelTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RdClient.Shared.Test.Helpers;
+using RdClient.Shared.ViewModels;
+using RdClient.Shared.Models;
+using RdClient.Shared.Navigation;
+
+namespace RdClient.Shared.Test.ViewModels
+{
+    [TestClass]
+    public class ConnectionCenterViewModelTests
+    {
+        private TestData _testData;
+        private Mock.DataModel _dataModel;
+        private Mock.NavigationService _navService;
+        private ConnectionCenterViewModel _vm;
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            _testData = new TestData();
+            _dataModel = new Mock.DataModel();
+            _dataModel.Desktops = new ModelCollection<Desktop>();
+            _vm = new ConnectionCenterViewModel();
+            ((IViewModel)_vm).Presenting(_navService, null, null);
+            _vm.DataModel = _dataModel;
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            _navService.Dispose();
+            _dataModel.Dispose();
+        }
+
+        [TestMethod]
+        public void TestAddDesktopExecute()
+        {
+            
+        }
+    }
+}
---
+++ RdClient/RdClient.Shared.Test/ViewModels/DesktopViewModelTests.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RdClient.Shared.Models;
+using RdClient.Shared.Test.Helpers;
+using RdClient.Shared.ViewModels;
+using System;
+using System.Collections.Generic;
+
+namespace RdClient.Shared.Test.ViewModels
+{
+    [TestClass]
+    public class DesktopViewModelTests
+    {
+        private TestData _testData;
+        private Mock.DataModel _dataModel;
+        private Mock.NavigationService _navService;
+        private Desktop _desktop;
+        private Credentials _cred;
+        private DesktopViewModel _vm;
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            _testData = new TestData();
+            _dataModel = new Mock.DataModel();
+            _dataModel.Credentials = new ModelCollection<Credentials>();
+            _dataModel.Desktops = new ModelCollection<Desktop>();
+            _dataModel.Thumbnails = new ModelCollection<Thumbnail>();
+            _navService = new Mock.NavigationService();
+            _cred = _testData.NewValidCredential();
+            _desktop = _testData.NewValidDesktop(_cred.Id);
+            _dataModel.Credentials.Add(_cred);
+            _dataModel.Desktops.Add(_desktop);
+            _vm = new DesktopViewModel(_desktop, _navService, _dataModel, null);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            _navService.Dispose();
+            _dataModel.Dispose();
+        }
+
+        [TestMethod]
+        public void TestDesktopMatches()
+        {
+            Assert.AreEqual(_desktop, _vm.Desktop);
+        }
+
+        [TestMethod]
+        public void TestCredentialReturnsNullIfDesktopHasNoCredential()
+        {
+            _desktop.CredentialId = Guid.Empty;
+            Assert.IsNull(_vm.Credential);
+        }
+
+        [TestMethod]
+        public void TestCredentialReturnsCredentialForDesktop()
+        {
+            Assert.AreEqual(_cred, _vm.Credential);
+        }
+
+        [TestMethod]
+        public void TestIsSelectedInitiallyFalse()
+        {
+            Assert.IsFalse(_vm.IsSelected);
+        }
+
+        [TestMethod]
+        public void TestIsSelectedSetCorrectly()
+        {
+            _vm.IsSelected = true;
+            Assert.IsTrue(_vm.IsSelected);
+            _vm.IsSelected = false;
+            Assert.IsFalse(_vm.IsSelected);
+        }
+
+        [TestMethod]
+        public void TestThumbnailCreatedCorrectly()
+        {
+            Assert.IsNotNull(_vm.Thumbnail);
+            Assert.AreEqual(_vm.Thumbnail.Id, _desktop.ThumbnailId);
+        }
+
+        [TestMethod]
+        public void TestEditCommandExecute()
+        {
+
+            _navService.Expect("PushModalView", new List<object> { "AddOrEditDesktopView", null, null }, 0);
+            _vm.EditCommand.Execute(null);
+        }
+
+        [TestMethod]
+        public void TestConnectCommandExecuteNavigatesToSessionViewIfCredentialsExist()
+        {
+            _navService.Expect("NavigateToView", new List<object> { "SessionView", null }, 0);
+            _vm.ConnectCommand.Execute(null);
+        }
+
+        [TestMethod]
+        public void TestConnectCommandExecuteShowsAddUserViewIfNoCredentials()
+        {
+            _vm.Desktop.CredentialId = Guid.Empty;
+            _navService.Expect("PushModalView", new List<object> { "AddUserView", null, null }, 0);
+            _vm.ConnectCommand.Execute(null);
+        }
+
+        [TestMethod]
+        public void TestDeleteCommandExecute()
+        {
+            _navService.Expect("PushModalView", new List<object> { "DeleteDesktopsView", null, null }, 0);
+            _vm.DeleteCommand.Execute(null);
+        }
+    }
+}
