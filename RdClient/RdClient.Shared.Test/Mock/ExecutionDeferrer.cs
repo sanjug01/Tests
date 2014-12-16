a17c7e785ce5238f6affe6586f6316cf4ebd2c52
Diman Todorov <diman.todorov@microsoft.com>
2014-12-16 13:50:37 -0800

    writing mouse view model tests

---
+++ RdClient/RdClient.Shared.Test/Mock/ExecutionDeferrer.cs
@@ -0,0 +1,19 @@
+﻿using RdClient.Shared.Navigation.Extensions;
+using RdMock;
+using System;
+
+namespace RdClient.Shared.Test.Mock
+{
+    public class ExecutionDeferrer : MockBase, IExecutionDeferrer
+    {
+        public bool TryDeferToUI(Action action)
+        {
+            return (bool)Invoke(new object[] { action });
+        }
+
+        public void DeferToUI(Action action)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RdClient/RdClient.Shared.Test/ViewModels/MouseViewModelTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RdClient.Shared.CxWrappers;
+using RdClient.Shared.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RdClient.Shared.Test.ViewModels
+{
+    [TestClass]
+    public class MouseViewModelTests
+    {
+
+        [TestMethod]
+        public void MouseViewModel_MousePointerChangedShouldSendMouseEvent()
+        {
+            RdpEventSource eventSource = new RdpEventSource();
+            using (Mock.RdpConnection connection = new Mock.RdpConnection(eventSource))
+            {
+                MouseViewModel mvm = new MouseViewModel();
+
+                mvm.RdpConnection = connection;
+
+                connection.Expect("SendMouseEvent", new List<object> { MouseEventType.MouseWheel, (float)0.5, (float)0.5 }, null);
+                mvm.MousePointer = new Tuple<int, float, float>(3, (float) 0.5, (float) 0.5);
+
+                Assert.AreEqual(new Tuple<float, float>((float) 0.5, (float) 0.5), mvm.MousePointerShapePosition);
+            }
+        }
+    }
+}
