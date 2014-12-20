90f33609e597ef582979dc5f551f7bf4cf879b92
James Dudley <jamesdu@microsoft.com>
2014-12-19 16:25:51 -0800

    Start styling settings view

--- RdClient/RdClient.Shared/ViewModels/SettingsViewModel.cs
+++ RdClient/RdClient.Shared/ViewModels/SettingsViewModel.cs
@@ -5,6 +5,9 @@ namespace RdClient.Shared.ViewModels
     public class SettingsViewModel : ViewModelBase 
     {
         private RelayCommand _goBackCommand;
+        private bool _showGeneralSettings;
+        private bool _showGatewaySettings;
+        private bool _showUserSettings;
 
         public SettingsViewModel()
         {
@@ -14,6 +17,32 @@ namespace RdClient.Shared.ViewModels
         public ICommand GoBackCommand
         {
             get { return _goBackCommand; }
+        }
+
+        public bool ShowGeneralSettings
+        {
+            get { return _showGeneralSettings; }
+            set { SetProperty(ref _showGeneralSettings, value); }
+        }
+
+        public bool ShowGatewaySettings
+        {
+            get { return _showGatewaySettings; }
+            set { SetProperty(ref _showGatewaySettings, value); }
+        }
+
+        public bool ShowUserSettings
+        {
+            get { return _showUserSettings; }
+            set { SetProperty(ref _showUserSettings, value); }
+        }
+
+        protected override void OnPresenting(object activationParameter)
+        {
+            base.OnPresenting(activationParameter);
+            this.ShowGeneralSettings = true;
+            this.ShowGatewaySettings = false;
+            this.ShowUserSettings = false;
         }
 
         private void GoBackCommandExecute(object parameter)
