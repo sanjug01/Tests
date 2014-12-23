67d863f33bb5109eb9feb9d686eb3134793993b0
Pavel Karpenko <pakarpen@microsoft.com>
2014-12-22 17:11:44 -0800

    Add basic unit tests for CoreWindowKeyboardCapture and KeystrokeEventArgs

---
+++ RdClient/RdClient.Shared.Test/Input/KeystrokeEventArgsTests.cs
@@ -0,0 +1,19 @@
+﻿namespace RdClient.Shared.Test.Input
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RdClient.Shared.Input;
+
+    [TestClass]
+    public sealed class KeystrokeEventArgsTests
+    {
+        [TestMethod]
+        public void NewKeystrokeEventArgs_CorrectPropertiesSet()
+        {
+            KeystrokeEventArgs e = new KeystrokeEventArgs(32, true, true, true);
+            Assert.AreEqual(32, e.KeyCode);
+            Assert.IsTrue(e.IsScanCode);
+            Assert.IsTrue(e.IsExtendedKey);
+            Assert.IsTrue(e.IsKeyReleased);
+        }
+    }
+}
