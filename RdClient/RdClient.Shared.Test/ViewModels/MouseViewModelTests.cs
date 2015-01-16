1e1a0f9b7b4889936384a238dff049df0f3c819a
Diman Todorov <Diman.Todorov@microsoft.com>
2015-01-16 14:03:58 -0800

    fixed a test

--- RdClient/RdClient.Shared.Test/ViewModels/MouseViewModelTests.cs
+++ RdClient/RdClient.Shared.Test/ViewModels/MouseViewModelTests.cs
@@ -1,30 +1,32 @@
-﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using RdClient.Shared.CxWrappers;
-//using RdClient.Shared.ViewModels;
-//using System;
-//using System.Collections.Generic;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RdClient.Shared.CxWrappers;
+using RdClient.Shared.ViewModels;
+using System.Collections.Generic;
+using Windows.Foundation;
 
-//namespace RdClient.Shared.Test.ViewModels
-//{
-//    [TestClass]
-//    public class MouseViewModelTests
-//    {
+namespace RdClient.Shared.Test.ViewModels
+{
+    [TestClass]
+    public class MouseViewModelTests
+    {
 
-//        [TestMethod]
-//        public void MouseViewModel_MousePointerChangedShouldSendMouseEvent()
-//        {
-//            RdpEventSource eventSource = new RdpEventSource();
-//            using (Mock.RdpConnection connection = new Mock.RdpConnection(eventSource))
-//            {
-//                MouseViewModel mvm = new MouseViewModel();
+        [TestMethod]
+        public void MouseViewModel_MousePointerChangedShouldSendMouseEvent()
+        {
+            RdpEventSource eventSource = new RdpEventSource();
+            using (Mock.RdpConnection connection = new Mock.RdpConnection(eventSource))
+            {
+                MouseViewModel mvm = new MouseViewModel();
 
-//                mvm.RdpConnection = connection;
+                mvm.RdpConnection = connection;
 
-//                connection.Expect("SendMouseEvent", new List<object> { MouseEventType.MouseWheel, (float)0.5, (float)0.5 }, null);
-//                mvm.MousePointer = new Tuple<int, float, float>(3, (float) 0.5, (float) 0.5);
+                connection.Expect("SendMouseEvent", new List<object> { MouseEventType.MouseWheel, (float)10.0, (float)10.0 }, null);
 
-//                Assert.AreEqual(new Tuple<float, float>((float) 0.5, (float) 0.5), mvm.MousePointerShapePosition);
-//            }
-//        }
-//    }
-//}
+                mvm.MousePosition = new Point(10.0, 10.0);
+                mvm.SendMouseAction(MouseEventType.MouseWheel);
+
+                Assert.AreEqual(new Point(10.0, 10.0), mvm.MousePosition);
+            }
+        }
+    }
+}
