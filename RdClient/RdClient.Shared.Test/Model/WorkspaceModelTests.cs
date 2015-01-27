21a01a2f345cedb60cd2d8e9152c58491fc8957e
Pavel Karpenko <pakarpen@microsoft.com>
2015-01-26 18:44:46 -0800

    100% code coverage for the "Data" workspace; add basic unit tests for WorkspaceModel

---
+++ RdClient/RdClient.Shared.Test/Model/WorkspaceModelTests.cs
@@ -0,0 +1,46 @@
+﻿namespace RdClient.Shared.Test.Model
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RdClient.Shared.Data;
+    using RdClient.Shared.Models;
+    using RdClient.Shared.Test.Data;
+
+    [TestClass]
+    public sealed class WorkspaceModelTests
+    {
+        [TestMethod]
+        public void LocalWorkspace_LoadFromEmptyFolder_NewCleanWorkspace()
+        {
+            IStorageFolder folder = new MemoryStorageFolder();
+            IModelSerializer serializer = new SerializableModelSerializer();
+
+            WorkspaceModel<LocalWorkspaceModel> workspace = new WorkspaceModel<LocalWorkspaceModel>(folder, serializer);
+            Assert.IsNotNull(workspace.WorkspaceData);
+            Assert.IsNotNull(workspace.Credentials.Models);
+            Assert.IsNotNull(workspace.Connections.Models);
+        }
+
+        [TestMethod]
+        public void LocalWorkspace_LoadFromEmptyFolderChangeSave_Saved()
+        {
+            IStorageFolder folder = new MemoryStorageFolder();
+            IModelSerializer serializer = new SerializableModelSerializer();
+            int folderCount = 0;
+
+            WorkspaceModel<LocalWorkspaceModel> workspace = new WorkspaceModel<LocalWorkspaceModel>(folder, serializer);
+            Assert.IsFalse(workspace.Save.CanExecute(null));
+            workspace.Credentials.AddNewModel(new CredentialsModel() { Username = "User", Password = "Password", Domain = "Domain" });
+            Assert.IsTrue(workspace.Save.CanExecute(null));
+            workspace.Save.Execute(null);
+            Assert.IsFalse(workspace.Save.CanExecute(null));
+
+            foreach(string folderName in folder.GetFolders())
+            {
+                Assert.IsTrue(string.Equals("credentials", folderName) || string.Equals("connections", folderName));
+                ++folderCount;
+            }
+
+            Assert.AreEqual(2, folderCount);
+        }
+    }
+}
--- RdClient/RdClient.Shared/Models/WorkspaceModel.cs
+++ RdClient/RdClient.Shared/Models/WorkspaceModel.cs
@@ -4,10 +4,11 @@
     using RdClient.Shared.ViewModels;
     using System;
     using System.ComponentModel;
+    using System.Diagnostics.Contracts;
     using System.IO;
     using System.Windows.Input;
 
-    public abstract class WorkspaceModel<TWorkspaceData> : SerializableCompositionModel where TWorkspaceData : INotifyPropertyChanged
+    public sealed class WorkspaceModel<TWorkspaceData> : SerializableCompositionModel where TWorkspaceData : INotifyPropertyChanged, new()
     {
         private readonly IStorageFolder _folder;
         private readonly IModelSerializer _modelSerializer;
@@ -17,7 +18,7 @@
         private readonly IModelCollection<CredentialsModel> _credentials;
         private bool _modelDataModified;
 
-        protected WorkspaceModel(IStorageFolder folder, IModelSerializer modelSerializer)
+        public WorkspaceModel(IStorageFolder folder, IModelSerializer modelSerializer)
         {
             _folder = folder;
             _modelSerializer = modelSerializer;
@@ -28,9 +29,14 @@
                 if (null != stream)
                 {
                     _workspaceData = _modelSerializer.ReadModel<TWorkspaceData>(stream);
-                    _workspaceData.PropertyChanged += this.OnWorkspaceDataChanged;
+                }
+                else
+                {
+                    _workspaceData = new TWorkspaceData();
                 }
             }
+
+            _workspaceData.PropertyChanged += this.OnWorkspaceDataChanged;
 
             _credentials = PrimaryModelCollection<CredentialsModel>.Load(folder.CreateFolder("credentials"), modelSerializer);
             _credentials.Save.CanExecuteChanged += this.OnCanSaveChanged;
@@ -38,6 +44,15 @@
             _connections.Save.CanExecuteChanged += this.OnCanSaveChanged;
 
             _modelDataModified = false;
+        }
+
+        public TWorkspaceData WorkspaceData
+        {
+            get
+            {
+                Contract.Ensures(null != Contract.Result<TWorkspaceData>());
+                return _workspaceData;
+            }
         }
 
         public IModelCollection<CredentialsModel> Credentials { get { return _credentials; } }
