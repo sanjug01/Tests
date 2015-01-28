03c455c0081c9aed2ceaa199fb7fcc1deb70730a
Pavel Karpenko <pakarpen@microsoft.com>
2015-01-28 14:40:41 -0800

    Add the new application data model

---
+++ RdClient/RdClient.Shared/Models/ApplicationDataModel.cs
@@ -0,0 +1,75 @@
+﻿namespace RdClient.Shared.Models
+{
+    using RdClient.Shared.Data;
+    using RdClient.Shared.Helpers;
+    using System;
+    using System.Diagnostics.Contracts;
+
+    public sealed class ApplicationDataModel : MutableObject
+    {
+        private IStorageFolder _rootFolder;
+        private IModelSerializer _modelSerializer;
+        private CertificateTrust _certificateTrust;
+        private WorkspaceModel<LocalWorkspaceModel> _localWorkspace;
+
+        public IStorageFolder RootFolder
+        {
+            get { return _rootFolder; }
+
+            set
+            {
+                Contract.Assert(null == _rootFolder, "RootFolder may be set only once");
+                Contract.Assert(null != value, "RootFolder cannot be removed");
+
+                if(this.SetProperty(ref _rootFolder, value))
+                {
+                    ComposeDataModel();
+                }
+            }
+        }
+
+        public IModelSerializer ModelSerializer
+        {
+            get { return _modelSerializer; }
+
+            set
+            {
+                Contract.Assert(null == _modelSerializer, "ModelSerializer may be set only once");
+                Contract.Assert(null != value, "ModelSerializer cannot be removed");
+
+                if (this.SetProperty(ref _modelSerializer, value))
+                {
+                    ComposeDataModel();
+                }
+            }
+        }
+
+        public WorkspaceModel<LocalWorkspaceModel> LocalWorkspace
+        {
+            get { return _localWorkspace; }
+            private set { this.SetProperty(ref _localWorkspace, value); }
+        }
+
+        public ApplicationDataModel()
+        {
+        }
+
+        private void ComposeDataModel()
+        {
+            if (null != _rootFolder && null != _modelSerializer)
+            {
+                Contract.Assert(null == _localWorkspace);
+                Contract.Assert(null == _certificateTrust);
+                //
+                // Create all subcomponents of the data model. This is done only once, when both the root folder and model serializer
+                // have been set (in XAML).
+                //
+                this.LocalWorkspace = new WorkspaceModel<LocalWorkspaceModel>(_rootFolder.CreateFolder("LocalWorkspace"), _modelSerializer);
+                //
+                // Load the certificate trust.
+                //
+                this._certificateTrust = new CertificateTrust();
+            }
+        }
+    }
+}
