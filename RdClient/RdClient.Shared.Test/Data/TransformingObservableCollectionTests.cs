ff342c8dd01162d72122b2dd301d32a99fea3c74
Pavel Karpenko <pakarpen@microsoft.com>
2015-02-04 11:16:49 -0800

    Add the TransformingObservableCollection utility class

---
+++ RdClient/RdClient.Shared.Test/Data/TransformingObservableCollectionTests.cs
@@ -0,0 +1,34 @@
+﻿namespace RdClient.Shared.Test.Data
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RdClient.Shared.Data;
+    using System.Collections.ObjectModel;
+
+    [TestClass]
+    public sealed class TransformingObservableCollectionTests
+    {
+        private ObservableCollection<int> _trueSource;
+        private ReadOnlyObservableCollection<int> _source;
+
+        [TestInitialize]
+        public void SetUpTest()
+        {
+            _trueSource = new ObservableCollection<int>();
+            _source = new ReadOnlyObservableCollection<int>(_trueSource);
+        }
+
+        [TestCleanup]
+        public void TearDownTest()
+        {
+            _source = null;
+            _trueSource = null;
+        }
+
+        [TestMethod]
+        public void NewTransformingObservableCollection_EmptySource_EmptyCollection()
+        {
+            ReadOnlyObservableCollection<string> transformed = TransformingObservableCollection<int, string>.Create(_source, number => number.ToString());
+            Assert.AreEqual(0, transformed.Count);
+        }
+    }
+}
---
+++ RdClient/RdClient.Shared/Data/TransformingObservableCollection.cs
@@ -0,0 +1,90 @@
+﻿namespace RdClient.Shared.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Collections.Specialized;
+
+    public sealed class TransformingObservableCollection<TSource, TTransformed> : ReadOnlyObservableCollection<TTransformed>
+    {
+        private readonly Transformator _transformator;
+
+        private sealed class Transformator
+        {
+            private readonly ObservableCollection<TTransformed> _transformed;
+            private readonly Func<TSource, TTransformed> _transformation;
+
+            public ObservableCollection<TTransformed> Transformed
+            {
+                get { return _transformed; }
+            }
+
+            public Transformator(ReadOnlyObservableCollection<TSource> sourceCollection, Func<TSource, TTransformed> transformation)
+            {
+                _transformed = new ObservableCollection<TTransformed>();
+                _transformation = transformation;
+
+                foreach (TSource original in sourceCollection)
+                {
+                    _transformed.Add(transformation(original));
+                }
+
+                INotifyCollectionChanged ncc = sourceCollection;
+                ncc.CollectionChanged += this.OnSourceCollectionChanged;
+            }
+
+            private void OnSourceCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+            {
+                int index, count;
+
+                switch(e.Action)
+                {
+                    case NotifyCollectionChangedAction.Add:
+                        index = e.NewStartingIndex;
+                        foreach (TSource original in e.NewItems)
+                            _transformed.Insert(index++, _transformation(original));
+                        break;
+
+                    case NotifyCollectionChangedAction.Move:
+                        count = e.OldItems.Count;
+                        for (index = 0; index < count; ++index)
+                            _transformed.Move(e.NewStartingIndex + index, e.OldStartingIndex + index);
+                        break;
+
+                    case NotifyCollectionChangedAction.Remove:
+                        count = e.OldItems.Count;
+                        for (index = 0; index < count; ++index)
+                            _transformed.RemoveAt(e.OldStartingIndex);
+                        break;
+
+                    case NotifyCollectionChangedAction.Replace:
+                        index = e.NewStartingIndex;
+                        foreach (TSource original in e.NewItems)
+                            _transformed[index++] = _transformation(original);
+                        break;
+
+                    default:
+                        _transformed.Clear();
+                        foreach (TSource original in (IEnumerable<TSource>)sender)
+                            _transformed.Add(_transformation(original));
+                        break;
+                }
+            }
+        }
+
+        public static ReadOnlyObservableCollection<TTransformed> Create(
+            ReadOnlyObservableCollection<TSource> sourceCollection,
+            Func<TSource, TTransformed> transformation)
+        {
+            Transformator transformator = new Transformator(sourceCollection, transformation);
+
+            return new TransformingObservableCollection<TSource, TTransformed>(transformator);
+        }
+
+        private TransformingObservableCollection(Transformator transformator)
+            : base(transformator.Transformed)
+        {
+            _transformator = transformator;
+        }
+    }
+}
