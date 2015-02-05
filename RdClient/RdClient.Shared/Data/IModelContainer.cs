8fe79459b3dda48c7d8354be4c2c8855168d24de
Pavel Karpenko <pakarpen@microsoft.com>
2015-02-05 13:47:37 -0800

    Add comments for the persistent state enum

--- RdClient/RdClient.Shared/Data/IModelContainer.cs
+++ RdClient/RdClient.Shared/Data/IModelContainer.cs
@@ -3,11 +3,23 @@
     using System;
     using System.ComponentModel;
 
+    /// <summary>
+    /// Persistent status of the model in a model container
+    /// </summary>
     [DefaultValue(PersistentStatus.Clean)]
     public enum PersistentStatus
     {
+        /// <summary>
+        /// Model in the container is clean and does not need to be saved in persistent storage
+        /// </summary>
         Clean,
+        /// <summary>
+        /// Model in the container is new and hasn't yet been saved in persistent storage
+        /// </summary>
         New,
+        /// <summary>
+        /// Model in the container has been modified since it was loaded from persistent storage and needs to be saved
+        /// </summary>
         Modified
     }
 
