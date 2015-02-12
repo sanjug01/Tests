a2e20f2e5ee2f8e359508ecc4bf10e5f47b47a31
Diman Todorov <Diman.Todorov@microsoft.com>
2015-02-11 19:11:17 -0800

    addressed CR

--- RdClient/RdMock/MockExpectedCall.cs
+++ RdClient/RdMock/MockExpectedCall.cs
@@ -41,8 +41,7 @@ namespace RdMock
                 Type expectedType = expectedParameters[i].GetType();
                 Type actualType = actualParameters[i].ParameterType;
 
-                if(Assignable.IsAssignable(actualType, expectedType) == false)
-                //if (actualType.IsAssignableFrom(expectedType) == false)
+                if(actualType.GetTypeInfo().IsAssignableFrom(expectedType.GetTypeInfo()) == false)
                 {
                     throw new MockException(string.Format("Mock call to {0}() failed. Parameter {1} is of type {2} but expected type is {3}",
                                                         this.FunctionName, i, actualType.Name, expectedType.Name));
