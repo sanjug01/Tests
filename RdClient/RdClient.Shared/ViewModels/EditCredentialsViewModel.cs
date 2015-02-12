603cd9a6c1c204da16bf0efd41767700adbf91a3
Pavel Karpenko <pakarpen@microsoft.com>
2015-02-11 18:57:33 -0800

    Add presentation infrastructure to the credentials editor

--- RdClient/RdClient.Shared/ViewModels/EditCredentialsViewModel.cs
+++ RdClient/RdClient.Shared/ViewModels/EditCredentialsViewModel.cs
@@ -1,6 +1,30 @@
 ﻿namespace RdClient.Shared.ViewModels
 {
+    using RdClient.Shared.Navigation;
+    using System.Diagnostics.Contracts;
     using System.Windows.Input;
+
+    /// <summary>
+    /// Extension for INavigationService that presents the EditCredentialsView modally with
+    /// a presentation task.
+    /// </summary>
+    public static class EditCredentialsExtension
+    {
+        public static void EditCredentials(this INavigationService navigationService, IEditCredentialsTask task)
+        {
+            Contract.Assert(null != navigationService, "EditCredentials|cannot present with null navigation service");
+            Contract.Requires(null != task);
+
+            navigationService.PushModalView("EditCredentialsView", task,
+                new ModalPresentationCompletion((sender, e) =>
+                {
+                    if (null != e.Result)
+                        task.Completed(e.Result);
+                    else
+                        task.Cancelled();
+                }));
+        }
+    }
 
     public sealed class EditCredentialsViewModel : ViewModelBase, IEditCredentialsViewModel
     {
@@ -16,6 +40,7 @@
         private string _userName;
         private string _password;
         private bool _canDismiss;
+        private IEditCredentialsTask _task;
 
         public ICommand Cancel
         {
@@ -99,10 +124,18 @@
 
         protected override void OnPresenting(object activationParameter)
         {
+            Contract.Assert(null != activationParameter, "EditCredentialsViewModel|presented with a null parameter");
+            Contract.Assert(null == _task, "EditCredentialsViewModel|presented without dismissing previous task");
+            Contract.Ensures(null != _task);
+            _task = activationParameter as IEditCredentialsTask;
+            Contract.Assert(null != _task, string.Format("EditCredentialsViewModel|presented with an invalid parameter|{0}", activationParameter));
         }
 
         protected override void OnDismissed()
         {
+            Contract.Assert(null != _task, "EditCredentialsViewModel|dismissed without presenting");
+            Contract.Ensures(null == _task);
+            _task = null;
         }
 
         private void CancelView(object parameter)
---
+++ RdClient/RdClient.Shared/ViewModels/IEditCredentialsTask.cs
@@ -0,0 +1,13 @@
+﻿namespace RdClient.Shared.ViewModels
+{
+    /// <summary>
+    /// Interface of a task that the EditCredentialsView must perform.
+    /// An object implementing the interface is passed to EditCredentialsViewModel by the navigation service
+    /// as the presentation parameter.
+    /// </summary>
+    public interface IEditCredentialsTask
+    {
+        void Completed(object result);
+        void Cancelled();
+    }
+}
