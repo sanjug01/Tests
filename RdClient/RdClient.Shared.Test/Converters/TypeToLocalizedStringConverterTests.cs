5568f5df8c09affcd00c44c8722fca786770fd3f
Diman Todorov <Diman.Todorov@microsoft.com>
2015-02-16 18:01:04 -0800

    first version of enum converter

---
+++ RdClient/RdClient.Shared.Test/Converters/TypeToLocalizedStringConverterTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RdClient.Shared.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RdClient.Shared.Test.Converters
+{
+    enum TestEnum
+    {
+        TestValue,
+        AnotherTestValue
+    }
+
+    [TestClass]
+    public class TypeToLocalizedStringConverterTests
+    {
+        
+
+        [TestMethod]
+        public void TypeToLocalizedStringConverterTests_Enum()
+        {
+            TypeToLocalizedStringConverter ttlsc = new TypeToLocalizedStringConverter();
+            TestEnum te = TestEnum.TestValue;
+
+            Assert.AreEqual("TestEnum_TestValue", ttlsc.Convert(te, typeof(string), null, null));
+        }
+
+        [TestMethod]
+        public void TypeToLocalizedStringConverterTests_Enum_Invalid()
+        {
+            TypeToLocalizedStringConverter ttlsc = new TypeToLocalizedStringConverter();
+            TestEnum te = (TestEnum) 3;
+
+            Assert.AreEqual("TestEnum_3", ttlsc.Convert(te, typeof(string), null, null));
+        }
+    }
+}
---
+++ RdClient/RdClient.Shared/Converters/TypeToLocalizedStringConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace RdClient.Shared.Converters
+{
+    public class TypeToLocalizedStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string typeName = value.GetType().FullName.Split('.').Last();
+
+            return typeName + "_" + value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
