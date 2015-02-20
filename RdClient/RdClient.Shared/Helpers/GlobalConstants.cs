33d217cc7784e91285b3610c0cdff86ba38fbe87
Alexandru Jugravu <Alexandru.Jugravu@microsoft.com>
2015-02-20 12:51:45 -0800

    adding global constants

---
+++ RdClient/RdClient.Shared/Helpers/GlobalConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RdClient.Shared.Helpers
+{
+    public static class GlobalConstants
+    {
+        public static const ulong MaxDoubleTapUS = 300000; // microseconds      
+        public static const double DesiredDeceleration = 0.002;
+    }
+}
