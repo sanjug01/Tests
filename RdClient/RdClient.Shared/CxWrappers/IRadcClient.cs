1a6d6212333c7e41182f1e7fae004c1e2a9c0ca9
unknown <jamesdu@jamesdu-DEV8.northamerica.corp.microsoft.com>
2015-03-16 15:56:35 -0700

    create IRadcClient interface methods

---
+++ RdClient/RdClient.Shared/CxWrappers/IRadcClient.cs
@@ -0,0 +1,27 @@
+﻿using RdClient.Shared.CxWrappers.Errors;
+using RdClient.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RdClient.Shared.CxWrappers
+{
+    public enum RadcRefreshReason
+    {
+        AddRemoteAppAccount = 0,
+        EditFeeds = 1,
+        ManualRefresh = 2,
+        BackgroundRefresh = 3
+    }
+
+    public interface IRadcClient
+    {
+        void StartSubscribeToOnPremFeed(string url, CredentialsModel cred, Action<XPlatError> completionHandler = null);
+        void StartRemoveFeed(string url, Action<XPlatError> completionHandler = null);
+        void StartGetCachedFeeds(Action<XPlatError> completionHandler = null);
+        void StartRefreshFeeds(RadcRefreshReason reason, Action<XPlatError> completionHandler = null);
+        void SetBackgroundRefreshInterval(uint minutes);
+    }
+}
