ec3c85ed5a45a58dbdf08a437fcf6e123e9e9a80
Pavel Karpenko <pakarpen@microsoft.com>
2015-04-01 15:11:08 -0700

    Replace deprecated reveal password property of the password box with the new property and value converter

---
+++ RdClient/RdClient.Shared.Test/Converters/BooleanToPasswordRevealModeConverterTests.cs
@@ -0,0 +1,106 @@
+﻿namespace RdClient.Shared.Test.Converters
+{
+    using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+    using RdClient.Shared.Converters;
+    using System;
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Data;
+
+    [TestClass]
+    public sealed class BooleanToPasswordRevealModeConverterTests
+    {
+        [TestMethod]
+        public void NewBooleanToPasswordRevealModeConverter_CorrectDefaultMode()
+        {
+            BooleanToPasswordRevealModeConverter converter = new BooleanToPasswordRevealModeConverter();
+
+            Assert.AreEqual(PasswordRevealMode.Peek, converter.RevealMode);
+        }
+
+        [TestMethod]
+        public void NewBooleanToPasswordRevealModeConverter_SetMode_CorrectModeSet()
+        {
+            BooleanToPasswordRevealModeConverter converter = new BooleanToPasswordRevealModeConverter()
+            {
+                RevealMode = PasswordRevealMode.Visible
+            };
+
+            Assert.AreEqual(PasswordRevealMode.Visible, converter.RevealMode);
+        }
+
+        [TestMethod]
+        public void BooleanToPasswordRevealModeConverter_ConvertTrueNoTargetType_Converted()
+        {
+            IValueConverter converter = new BooleanToPasswordRevealModeConverter();
+
+            Assert.AreEqual(PasswordRevealMode.Peek, converter.Convert(true, null, null, null));
+        }
+
+        [TestMethod]
+        public void BooleanToPasswordRevealModeConverter_ConvertFalseNoTargetType_Converted()
+        {
+            IValueConverter converter = new BooleanToPasswordRevealModeConverter();
+
+            Assert.AreEqual(PasswordRevealMode.Hidden, converter.Convert(false, null, null, null));
+        }
+
+        [TestMethod]
+        public void BooleanToPasswordRevealModeConverter_ConvertTrueCorrectTargetType_Converted()
+        {
+            IValueConverter converter = new BooleanToPasswordRevealModeConverter();
+
+            Assert.AreEqual(PasswordRevealMode.Peek, converter.Convert(true, typeof(PasswordRevealMode), null, null));
+        }
+
+        [TestMethod]
+        public void BooleanToPasswordRevealModeConverter_ConvertFalseCorrectTargetType_Converted()
+        {
+            IValueConverter converter = new BooleanToPasswordRevealModeConverter();
+
+            Assert.AreEqual(PasswordRevealMode.Hidden, converter.Convert(false, typeof(PasswordRevealMode), null, null));
+        }
+
+        [TestMethod]
+        public void BooleanToPasswordRevealModeConverter_ConvertTrueBadTargetType_Throws()
+        {
+            IValueConverter converter = new BooleanToPasswordRevealModeConverter();
+
+            try
+            {
+                Assert.AreEqual(PasswordRevealMode.Peek, converter.Convert(true, typeof(bool), null, null));
+                Assert.Fail("Expected exception");
+            }
+            catch(ArgumentException)
+            {
+                //
+                // correct behavior
+                //
+            }
+            catch
+            {
+                Assert.Fail("Unexpected exception");
+            }
+        }
+
+        [TestMethod]
+        public void BooleanToPasswordRevealModeConverter_ConvertBack_Throws()
+        {
+            IValueConverter converter = new BooleanToPasswordRevealModeConverter();
+
+            try
+            {
+                converter.ConvertBack(PasswordRevealMode.Hidden, null, null, null);
+            }
+            catch(NotImplementedException)
+            {
+                //
+                // correct behavior
+                //
+            }
+            catch
+            {
+                Assert.Fail("Unexpected exception");
+            }
+        }
+    }
+}
---
+++ RdClient/RdClient.Shared/Converters/BooleanToPasswordRevealModeConverter.cs
@@ -0,0 +1,49 @@
+﻿namespace RdClient.Shared.Converters
+{
+    using System;
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Data;
+
+    /// <summary>
+    /// Converter that converts a boolean value to a configured value of the PasswordRevealMode enum.
+    /// </summary>
+    /// <remarks>The converter may be established in XAML and given the password reveal mode to return
+    /// for input boolean values equal to true.</remarks>
+    public sealed class BooleanToPasswordRevealModeConverter : IValueConverter
+    {
+        private PasswordRevealMode _revealMode;
+
+        /// <summary>
+        /// Value of the PasswordRevealMode enum returned by the converter for input boolean values equal to true.
+        /// </summary>
+        /// <remarks>Default reveal mode is PasswordRevealMode.Peek</remarks>
+        public PasswordRevealMode RevealMode
+        {
+            get { return _revealMode; }
+            set { _revealMode = value; }
+        }
+
+        public BooleanToPasswordRevealModeConverter()
+        {
+            _revealMode = PasswordRevealMode.Peek;
+        }
+
+        object IValueConverter.Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (null != targetType && !targetType.Equals(typeof(PasswordRevealMode)))
+                throw new ArgumentException("Target type must be PasswordRevealMode", "targetType");
+
+            if(!(value is bool))
+                throw new ArgumentException("Value type must be bool", "value");
+            //
+            // If the input value is true, return the configured value; otherwise, hide the reveal password UI.
+            //
+            return ((bool)value) ? _revealMode : PasswordRevealMode.Hidden;
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
