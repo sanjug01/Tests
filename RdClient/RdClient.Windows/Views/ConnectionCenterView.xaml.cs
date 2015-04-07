80a5d4285612fd88e62e886ab9927e6a5bfa6219
Pavel Karpenko <pakarpen@microsoft.com>
2015-04-07 13:32:51 -0700

    - Separate the toolbar and desktops/apps selector in the ConnectionCenterTitleBar control. - Add the visual state manager to the connection center view. - Add selection of an appropriate visual state in the connection center's code behind.

--- RdClient/RdClient.Windows/Views/ConnectionCenterView.xaml.cs
+++ RdClient/RdClient.Windows/Views/ConnectionCenterView.xaml.cs
@@ -1,26 +1,46 @@
-﻿using RdClient.Shared.Navigation;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-
-namespace RdClient.Views
+﻿namespace RdClient.Views
 {
+    using RdClient.Shared.Navigation;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+
     public sealed partial class ConnectionCenterView : Page, IPresentableView
     {
+        //
+        // The enum prevents typos in names of visual states. When a new visual state
+        // needs to be applied, the name of the state comes from a member of the enum.
+        //
+        private enum Layout
+        {
+            DefaultLayout,
+            NarrowLayout,
+            PhoneLayout
+        }
+
         public ConnectionCenterView()
         {
             this.InitializeComponent();
+            this.SizeChanged += this.OnSizeChanged;
         }
 
-        public IViewModel ViewModel
+        IViewModel IPresentableView.ViewModel { get { return this.DataContext as IViewModel; } }
+        void IPresentableView.Activating(object activationParameter) { }
+        void IPresentableView.Presenting(INavigationService navigationService, object activationParameter) { }
+        void IPresentableView.Dismissing() { }
+
+        private void OnSizeChanged(object sender, SizeChangedEventArgs e)
         {
-            get { return this.DataContext as IViewModel; }
+            //
+            // Analyze the layout and apply an appropriate visual state.
+            //
+            Layout layout = Layout.DefaultLayout;
+            //
+            // If the view has become narrower than 640 pixels, switch to the narrow layout.
+            //
+            if (e.NewSize.Width < 640)
+                layout = Layout.NarrowLayout;
+
+            VisualStateManager.GoToState(this, layout.ToString(), true);
         }
-
-        public void Activating(object activationParameter) { }
-
-        public void Presenting(INavigationService navigationService, object activationParameter) { }
-
-        public void Dismissing() { }
-
     }
 }
