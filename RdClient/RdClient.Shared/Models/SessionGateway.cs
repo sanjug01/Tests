c3b151842bbbae547a39b36feef243df81f128dd
unknown <aljugrav@ALJUGRAV-DEV2.northamerica.corp.microsoft.com>
2015-04-09 17:30:28 -0700

    first step in extending RemoteSessionSetup.

---
+++ RdClient/RdClient.Shared/Models/SessionGateway.cs
@@ -0,0 +1,122 @@
+﻿namespace RdClient.Shared.Models
+{
+    using RdClient.Shared.Data;
+    using System;
+    using System.ComponentModel;
+    using System.Diagnostics.Contracts;
+
+    /// <summary>
+    /// Information about gateway and assigned credentials model used by a particular session.
+    /// This object may be edited by the in-session credentials editing task.
+    /// </summary>
+    public sealed class SessionGateway
+    {
+        private readonly GatewayModel _gateway;
+        //
+        // Credentials object used by the session; this is always a new object, not stored
+        // in the data model.
+        //
+        private readonly CredentialsModel _credentials;
+        //
+        // Modification status of the password in _credentials - if the password has been
+        // edited by user after the credentials object was loaded from the data model,
+        // _modified is set to true. It remains true until a new credentials object
+        // is loaded form the data model.
+        //
+        private bool _newPassword;
+
+        public CredentialsModel Credentials
+        {
+            get { return _credentials; }
+        }
+
+        public GatewayModel Gateway
+        {
+            get { return _gateway; }
+        }
+
+        /// <summary>
+        /// True if the password was modified by user.
+        /// The property is sent to the RDC server so the server may reject the credentials
+        /// and require user to type the password.
+        /// </summary>
+        public bool IsNewPassword
+        {
+            get { return _newPassword; }
+        }
+
+        public void ApplySavedCredentials(IModelContainer<CredentialsModel> savedCredentials)
+        {
+            _credentials.PropertyChanged -= this.OnCredentialsPropertyChanged;
+            savedCredentials.Model.CopyTo(_credentials);
+            _newPassword = false;
+            _credentials.PropertyChanged += this.OnCredentialsPropertyChanged;
+        }
+
+        public void SetChangedPassword(string changedPassword)
+        {
+            _credentials.PropertyChanged -= this.OnCredentialsPropertyChanged;
+            _credentials.Password = changedPassword;
+            _newPassword = true;
+            _credentials.PropertyChanged += this.OnCredentialsPropertyChanged;
+        }
+
+        public Guid SaveCredentials(ApplicationDataModel dataModel)
+        {
+            Guid credentialsId = Guid.Empty;
+            //
+            //
+            //
+            foreach(IModelContainer<CredentialsModel> c in dataModel.Credentials.Models)
+            {
+                if(string.Equals(c.Model.Username, _credentials.Username, StringComparison.OrdinalIgnoreCase))
+                {
+                    credentialsId = c.Id;
+                    _credentials.CopyTo(c.Model);
+                    Contract.Assert(!Guid.Empty.Equals(credentialsId));
+                    break;
+                }
+            }
+
+            if (Guid.Empty.Equals(credentialsId))
+            {
+                CredentialsModel newCredentials = new CredentialsModel(_credentials);
+                credentialsId = dataModel.Credentials.AddNewModel(newCredentials);
+            }
+
+            return credentialsId;
+        }
+
+        public SessionGateway()
+        {
+            _gateway = new GatewayModel();
+            _credentials = new CredentialsModel();
+            //
+            // There is no loaded model, so any password typed in will be modified.
+            //
+            _newPassword = true;
+            _credentials.PropertyChanged += this.OnCredentialsPropertyChanged;
+        }
+
+        public SessionGateway(
+            IModelContainer<GatewayModel> savedGateway,
+            IModelContainer<CredentialsModel> savedCredentials
+            )
+        {
+            Contract.Assert(null != savedCredentials);
+
+            _gateway = new GatewayModel();
+            _credentials = new CredentialsModel();
+
+            savedGateway.Model.CopyTo(_gateway);
+            savedCredentials.Model.CopyTo(_credentials);
+            _newPassword = false;
+            _credentials.PropertyChanged += this.OnCredentialsPropertyChanged;
+        }
+
+        private void OnCredentialsPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            _newPassword = true;
+        }
+    }
+}
