0fad144185852bffcab139e64ba315fce6656baa
unknown <ditodoro@WIN-O4S4LOSKVDB.europe.corp.microsoft.com>
2015-04-10 16:26:26 -0700

    added point tracker, zoom/scroll recognizer

---
+++ RdClient/RdClient.Shared/Input/Pointer/PointTracker.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.Foundation;
+
+namespace RdClient.Shared.Input
+{
+    public class PointTrackerException : Exception
+    {
+        public PointTrackerException(string message) : base(message)
+        {
+
+        }
+    }
+
+    public class PointTracker : IPointTracker
+    {
+        private Dictionary<UInt32, Point> _trackedPoints = new Dictionary<UInt32, Point>();
+        private LinkedList<UInt32> _pointSequence = new LinkedList<UInt32>();
+
+        public void Track(Point point, UInt32 id)
+        {
+            if(_pointSequence.Contains(id) == false)
+            {
+                _pointSequence.AddLast(id);
+            }
+
+            _trackedPoints[id] = new Point() { X = point.X, Y = point.Y };
+        }
+
+        public void Untrack(UInt32 id)
+        {
+            if(_pointSequence.Contains(id))
+            {
+                _pointSequence.Remove(id);
+                _trackedPoints.Remove(id);
+            }
+        }
+
+        public bool IsTracked(UInt32 id)
+        {
+            return _trackedPoints.ContainsKey(id);
+        }
+
+        public int Contacts
+        {
+            get
+            {
+                return _pointSequence.Count;
+            }
+        }
+
+        public Point Center
+        {
+            get
+            {
+                if(_pointSequence.Count == 0)
+                {
+                    throw new PointTrackerException("no contacts tracked");
+                }
+                else if(_pointSequence.Count == 1)
+                {
+                    return _trackedPoints[_pointSequence.First.Value];
+                }
+                else
+                {
+                    return new Point()
+                    {
+                        X = (_trackedPoints[_pointSequence.ElementAt(0)].X + _trackedPoints[_pointSequence.ElementAt(1)].X) / 2,
+                        Y = (_trackedPoints[_pointSequence.ElementAt(0)].Y + _trackedPoints[_pointSequence.ElementAt(1)].Y) / 2
+                    };
+                }
+            }
+        }
+    }
+}
---
+++ RdClient/RdClient.Shared/Input/Pointer/ZoomScrollEvent.cs
@@ -0,0 +1,27 @@
+﻿using Windows.Foundation;
+using Windows.UI.Input;
+
+namespace RdClient.Shared.Input
+{
+    public class ZoomScrollEvent : IZoomScrollEvent
+    {
+        public PointerEventAction Action { get; private set; }
+
+		public Point Position { get; private set; }
+
+        public ManipulationDelta Delta { get; private set; }
+
+        public bool IsInertial { get; private set; }
+
+        public ZoomScrollType Type { get; private set; }
+
+		public ZoomScrollEvent(PointerEventAction action, ZoomScrollType type, ManipulationDelta delta, bool isInertial, Point position)
+        {
+            Action = action;
+            Type = type;
+            Delta = delta;
+            IsInertial = isInertial;
+            Position = position;
+        }
+    }
+}
