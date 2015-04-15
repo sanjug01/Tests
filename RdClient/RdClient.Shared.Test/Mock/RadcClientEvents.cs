56e55dd23147d82b9aeab552f6fa27ac34b30cfa
James Dudley <jamesdu@microsoft.com>
2015-04-15 16:30:21 -0700

    Add unit tests for HasApps / HasDesktops / ShowSectionLabels

---
+++ RdClient/RdClient.Shared.Test/Mock/RadcClientEvents.cs
@@ -0,0 +1,17 @@
+﻿namespace RdClient.Shared.Test.Mock
+{
+    using RdClient.Shared.CxWrappers;
+    using RdMock;
+    using System;
+
+    public class RadcClientEvents : MockBase, IRadcEvents
+    {
+        #pragma warning disable 67 // warning CS0067: the event <...> is never used - they shouldn't be as this is a mock object
+        public event EventHandler<RadcAddResourcesFinishedArgs> AddResourcesFinished;
+        public event EventHandler<RadcAddResourcesStartedArgs> AddResourcesStarted;
+        public event EventHandler<RadcOperationCompletedArgs> OperationCompleted;
+        public event EventHandler<RadcOperationInProgressArgs> OperationInProgress;
+        public event EventHandler<RadcResourceAddedArgs> ResourceAdded;
+        public event EventHandler<RadcWorkspaceRemovedArgs> WorkspaceRemoved;
+    }
+}
