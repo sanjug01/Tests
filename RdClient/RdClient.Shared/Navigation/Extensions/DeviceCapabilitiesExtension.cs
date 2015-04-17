33b2b652c9a906fe471e719c1aef2acbda18ca23
Pavel Karpenko <pakarpen@microsoft.com>
2015-04-16 17:23:38 -0700

    Add a navigation extension that injects a device capabilities object in view models.

---
+++ RdClient/RdClient.Shared/Navigation/Extensions/DeviceCapabilitiesExtension.cs
@@ -0,0 +1,26 @@
+﻿namespace RdClient.Shared.Navigation.Extensions
+{
+    using RdClient.Shared.Helpers;
+    using RdClient.Shared.Models;
+
+    public sealed class DeviceCapabilitiesExtension : MutableObject, INavigationExtension
+    {
+        private IDeviceCapabilities _deviceCapabilities;
+
+        public IDeviceCapabilities DeviceCapabilities
+        {
+            get { return _deviceCapabilities; }
+            set { this.SetProperty(ref _deviceCapabilities, value); }
+        }
+
+        void INavigationExtension.Presenting(IViewModel viewModel)
+        {
+            viewModel.CastAndCall<IDeviceCapabilitiesSite>(site => site.SetDeviceCapabilities(_deviceCapabilities));
+        }
+
+        void INavigationExtension.Dismissed(IViewModel viewModel)
+        {
+            viewModel.CastAndCall<IDeviceCapabilitiesSite>(site => site.SetDeviceCapabilities(null));
+        }
+    }
+}
