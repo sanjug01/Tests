ff216e9541f4b2fdb426778220bd389e7701b86e
Alex J <aljugravu@microsoft.com>
2015-04-30 14:23:06 -0700

    basic view model for the RichTextView

---
+++ RdClient/RdClient.Shared/ViewModels/RichTextViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace RdClient.Shared.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System.Windows.Input;
+    using Windows.ApplicationModel;
+    using Windows.UI.Xaml;
+
+    public sealed class RichTextViewModel : AccessoryViewModelBase
+    {
+        private readonly RelayCommand _closeCommand;
+
+        public RichTextViewModel()
+        {
+            _closeCommand = new RelayCommand(o => { this.DismissModal(null); });
+        }
+
+        public ICommand Close { get { return _closeCommand; } }
+
+        protected override void DefaultAction()
+        {
+            DismissModal(null);
+        }
+
+    }
+}
--- RdClient/RdClient.Windows/Views/RichTextView.xaml.cs
+++ RdClient/RdClient.Windows/Views/RichTextView.xaml.cs
@@ -9,21 +9,31 @@
     {
         public RichTextView()
         {
+            // TODO: _resourceFile should be bindable
+            _resourceFileName = "ms-appx:///Strings/EULA.rtf";
             this.InitializeComponent();
         }
 
-        private async void RichEditBox_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        private void RichEditBox_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            this.LoadResourceFile();
+        }
+
+        private async void LoadResourceFile()
         {
             StorageFile infoFile = await StorageFile.GetFileFromApplicationUriAsync(
-                new Uri("ms-appx:///Strings/EULA.rtf"));
+                new Uri(_resourceFileName));
             _infoText = await FileIO.ReadTextAsync(infoFile);
 
             // apply text
             this.InfoBox.IsReadOnly = false;
             this.InfoBox.Document.SetText(Windows.UI.Text.TextSetOptions.FormatRtf, _infoText);
             this.InfoBox.IsReadOnly = true;
+            // bug with the foreground color
+            this.InfoBox.Focus(Windows.UI.Xaml.FocusState.Programmatic);
         }
 
+        private string _resourceFileName;
         private string _infoText;
     }
 }
