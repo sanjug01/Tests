80ceac44e3d8f50aa25a5412c046d4615ee52845
James Dudley <jamesdu@microsoft.com>
2015-05-08 16:25:57 -0700

    get hostnamevalidationruletests passing

--- RdClient/RdClient.Shared.Test/ValidationRules/HostNameValidationRuleTests.cs
+++ RdClient/RdClient.Shared.Test/ValidationRules/HostNameValidationRuleTests.cs
@@ -9,82 +9,82 @@ using RdClient.Shared.ValidationRules;
 
 namespace RdClient.Shared.Test.ValidationRules
 {
-    //[TestClass]
-    //public class HostNameValidationRuleTests
-    //{
-    //    // list of illegal caracters 
-    //    private const string _illegalCharacters = "`~!#@$%^&*()=+{}\\|;'\",< >/?";
+    [TestClass]
+    public class HostNameValidationRuleTests
+    {
+        // list of illegal characters 
+        private const string _illegalCharacters = "`~!#@$%^&*()=+{}\\|;'\",< >/?";
 
-    //    [TestMethod]
-    //    public void ValidHostNames_ShouldBeValidated()
-    //    {
-    //        HostNameValidationRule rule = new HostNameValidationRule();
+        [TestMethod]
+        public void ValidHostNames_ShouldBeValidated()
+        {
+            HostNameValidationRule rule = new HostNameValidationRule();
 
-    //        string hostName = "aBC";
-    //        Assert.IsTrue(rule.Validate(hostName, CultureInfo.CurrentCulture));
+            string hostName = "aBC";
+            Assert.IsTrue(rule.Validate(hostName).IsValid);
 
-    //        hostName = "abc.mydomain.com";
-    //        Assert.IsTrue(rule.Validate(hostName, CultureInfo.CurrentCulture));
+            hostName = "abc.mydomain.com";
+            Assert.IsTrue(rule.Validate(hostName).IsValid);
 
-    //        hostName = "_myHost123Cd";
-    //        Assert.IsTrue(rule.Validate(hostName, CultureInfo.CurrentCulture));
+            hostName = "_myHost123Cd";
+            Assert.IsTrue(rule.Validate(hostName).IsValid);
 
-    //        hostName = "23.24.11.22";
-    //        Assert.IsTrue(rule.Validate(hostName, CultureInfo.CurrentCulture));
+            hostName = "23.24.11.22";
+            Assert.IsTrue(rule.Validate(hostName).IsValid);
 
-    //        hostName = "mypc.com";
-    //        Assert.IsTrue(rule.Validate(hostName, CultureInfo.CurrentCulture));
-    //    }
+            hostName = "mypc.com";
+            Assert.IsTrue(rule.Validate(hostName).IsValid);
+        }
 
-    //    [TestMethod]
-    //    public void HostNamesHasInvalidCharacters_ShouldFailValidation()
-    //    {
-    //        HostNameValidationRule rule = new HostNameValidationRule();
+        [TestMethod]
+        public void HostNamesHasInvalidCharacters_ShouldFailValidation()
+        {
+            HostNameValidationRule rule = new HostNameValidationRule();
 
-    //        string prefix = "a1b";
-    //        string suffix = "_2df";
-    //        string core;
-    //        string hostName;
-    //        for(int i = 0 ; i< _illegalCharacters.Length ; i++)
-    //        {
-    //            core = _illegalCharacters[i].ToString();
-    //            hostName = prefix + core + suffix;
-    //            Assert.IsFalse(rule.Validate(hostName, CultureInfo.InvariantCulture));
-    //        }
-    //    }
+            string prefix = "a1b";
+            string suffix = "_2df";
+            string core;
+            string hostName;
+            for (int i = 0; i < _illegalCharacters.Length; i++)
+            {
+                core = _illegalCharacters[i].ToString();
+                hostName = prefix + core + suffix;
+                Assert.IsFalse(rule.Validate(hostName).IsValid);
+            }
+        }
 
-    //    [TestMethod]
-    //    public void HostNamesStartsWithInvalidCharacters_ShouldFailValidation()
-    //    {
-    //        HostNameValidationRule rule = new HostNameValidationRule();
+        [TestMethod]
+        public void HostNamesStartsWithInvalidCharacters_ShouldFailValidation()
+        {
+            HostNameValidationRule rule = new HostNameValidationRule();
 
-    //        string prefix ;
-    //        string suffix = "d3f";
-    //        string core = "xy_";
-    //        string hostName;
-    //        for (int i = 0; i < _illegalCharacters.Length; i++)
-    //        {
-    //            prefix = _illegalCharacters[i].ToString();
-    //            hostName = prefix + core + suffix;
-    //            Assert.IsFalse(rule.Validate(hostName, CultureInfo.InvariantCulture));
-    //        }
-    //    }
+            string prefix;
+            string suffix = "d3f";
+            string core = "xy_";
+            string hostName;
+            for (int i = 0; i < _illegalCharacters.Length; i++)
+            {
+                prefix = _illegalCharacters[i].ToString();
+                hostName = prefix + core + suffix;
+                Assert.IsFalse(rule.Validate(hostName).IsValid);
+            }
+        }
 
-    //    [TestMethod]
-    //    public void HostNameEndsWithInvalidCharacters_ShouldFailValidation()
-    //    {
-    //        HostNameValidationRule rule = new HostNameValidationRule();
+        [TestMethod]
+        public void HostNameEndsWithInvalidCharacters_ShouldFailValidation()
+        {
+            HostNameValidationRule rule = new HostNameValidationRule();
 
-    //        string prefix="pq";
-    //        string suffix ;
-    //        string core = "_tv_";
-    //        string hostName;
-    //        for (int i = 0; i < _illegalCharacters.Length; i++)
-    //        {
-    //            suffix = _illegalCharacters[i].ToString();
-    //            hostName = prefix + core + suffix;
-    //            Assert.IsFalse(rule.Validate(hostName, CultureInfo.CurrentCulture));
-    //        }
-    //    }
-    //}
+            string prefix = "pq";
+            string suffix;
+            string core = "_tv_";
+            string hostName;
+            for (int i = 0; i < _illegalCharacters.Length; i++)
+            {
+                suffix = _illegalCharacters[i].ToString();
+                hostName = prefix + core + suffix;
+                Assert.IsFalse(rule.Validate(hostName).IsValid);
+            }
+        }
+    }
 }
