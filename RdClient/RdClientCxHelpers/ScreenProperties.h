0f31516dc0f47a993a001a4ddc53dd141bc26519
unknown <ditodoro@WIN-LPSF2IT642M.europe.corp.microsoft.com>
2015-05-08 14:47:16 -0700

    removed height cap

---
+++ RdClient/RdClientCxHelpers/ScreenProperties.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+namespace RdClientCxHelpers
+{
+	inline void ThrowIfFailed(HRESULT hr)
+	{
+		if (FAILED(hr))
+		{
+			// Set a breakpoint on this line to catch DirectX API errors
+			throw Platform::Exception::CreateException(hr);
+		}
+	}
+
+	public ref class ScreenProperties sealed
+	{
+	public:
+		ScreenProperties();
+		property Windows::Foundation::Point Resolution
+		{
+			Windows::Foundation::Point get();
+		}
+
+	private:
+		D3D_FEATURE_LEVEL							    m_featureLevel;
+		Microsoft::WRL::ComPtr<ID3D11Device1>           m_d3dDevice;
+	};
+}
---
+++ RdClient/RdClientCxHelpers/pch.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+#include <wrl/client.h>
+#include <d3d11_1.h>
+#include <d2d1_1.h>
+#include <d2d1effects.h>
+#include <dwrite_1.h>
+#include <wincodec.h>
