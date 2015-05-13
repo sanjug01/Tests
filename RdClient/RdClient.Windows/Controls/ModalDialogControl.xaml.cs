6a9a0a17e445de0edba3c60f1129db30fb485eaa
James Dudley <jamesdu@microsoft.com>
2015-05-12 18:49:17 -0700

    Change name of dynamic dialog control

---
+++ RdClient/RdClient.Windows/Controls/ModalDialogControl.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace RdClient.Controls
+{
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+
+    public sealed partial class ModalDialogControl : UserControl
+    {
+        public ModalDialogControl()
+        {
+            this.InitializeComponent();
+        }
+
+        static ModalDialogControl()
+        {
+            DialogContentProperty = DependencyProperty.Register("DialogContent", typeof(UIElement), typeof(ModalDialogControl), new PropertyMetadata(null, DialogContentChanged));
+        }
+
+        public UIElement DialogContent
+        {
+            set { SetValue(DialogContentProperty, value); }
+        }
+
+        public static readonly DependencyProperty DialogContentProperty;
+
+        private static void DialogContentChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            (d as ModalDialogControl).DialogPresenter.Content = e.NewValue as UIElement;
+        }
+    }
+}
