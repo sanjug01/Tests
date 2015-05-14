0a3fa8c51afa04f40065c34d7eeab35dc94fd326
Alex J <aljugrav@microsoft.com>
2015-05-13 18:33:09 -0700

    introducing order for the combobox elements

--- RdClient/RdClient.Shared/ViewModels/GatewayComboBoxElement.cs
+++ RdClient/RdClient.Shared/ViewModels/GatewayComboBoxElement.cs
@@ -2,12 +2,36 @@
 {
     using RdClient.Shared.Data;
     using RdClient.Shared.Models;
+    using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
 
     public enum GatewayComboBoxType
     {
-        Gateway,
-        None,
-        AddNew
+        AddNew = 0,
+        None = 1,
+        Gateway = 2
+    }
+
+    public sealed class GatewayComboBoxOrder : IComparer<GatewayComboBoxElement>
+    {
+        public int Compare(GatewayComboBoxElement x, GatewayComboBoxElement y)
+        {
+            Contract.Assert(null != x);
+            Contract.Assert(null != y);
+
+            int compareType = x.GatewayComboBoxType < y.GatewayComboBoxType ? -1 : x.GatewayComboBoxType > y.GatewayComboBoxType ? 1 : 0;
+
+            if (0 == compareType && GatewayComboBoxType.Gateway == x.GatewayComboBoxType)
+            {
+                // compare by gateway hostname if both have gateways
+                Contract.Assert(null != x.Gateway && null != x.Gateway.Model);
+                Contract.Assert(null != y.Gateway && null != y.Gateway.Model);
+
+                return string.Compare(x.Gateway.Model.HostName, y.Gateway.Model.HostName);
+            }
+
+            return compareType;
+        }
     }
 
     public class GatewayComboBoxElement
--- RdClient/RdClient.Shared/ViewModels/UserComboBoxElement.cs
+++ RdClient/RdClient.Shared/ViewModels/UserComboBoxElement.cs
@@ -2,13 +2,39 @@
 {
     using RdClient.Shared.Data;
     using RdClient.Shared.Models;
+    using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
+    using System;
 
     public enum UserComboBoxType
     {
-        Credentials,
-        AskEveryTime,
-        AddNew
+        AddNew = 0,
+        AskEveryTime = 1,
+        Credentials = 2
     }
+
+    public sealed class UserComboBoxOrder : IComparer<UserComboBoxElement>
+    {
+        public int Compare(UserComboBoxElement x, UserComboBoxElement y)
+        {
+            Contract.Assert(null != x);
+            Contract.Assert(null != y);
+
+            int compareType = x.UserComboBoxType < y.UserComboBoxType ? -1 : x.UserComboBoxType > y.UserComboBoxType ? 1 : 0;
+
+            if (0 == compareType && UserComboBoxType.Credentials == x.UserComboBoxType)
+            {
+                // compare by user name if both have credentials
+                Contract.Assert(null != x.Credentials && null != x.Credentials.Model);
+                Contract.Assert(null != y.Credentials && null != x.Credentials.Model);
+
+                return string.Compare(x.Credentials.Model.Username, y.Credentials.Model.Username);
+            }
+
+            return compareType;    
+        }
+    }
+
 
     public class UserComboBoxElement
     {
