ed9b66971d0c31a555e0b056f4800bf52a2b1d90
Diman Todorov <ditodoro@WIN-LPSF2IT642M.europe.corp.microsoft.com>
2015-06-04 11:17:11 -0700

    fix for screenproperties leak

--- RdClient/RdClientCxHelpers/ScreenProperties.h
+++ RdClient/RdClientCxHelpers/ScreenProperties.h
@@ -15,6 +15,8 @@ namespace RdClientCxHelpers
 	{
 	public:
 		ScreenProperties();
+		~ScreenProperties();
+
 		property Windows::Foundation::Size Resolution
 		{
 			Windows::Foundation::Size get();
@@ -23,5 +25,6 @@ namespace RdClientCxHelpers
 	private:
 		D3D_FEATURE_LEVEL							    m_featureLevel;
 		Microsoft::WRL::ComPtr<ID3D11Device1>           m_d3dDevice;
+		Microsoft::WRL::ComPtr<ID2D1Factory>            m_d2dFactory;
 	};
 }
