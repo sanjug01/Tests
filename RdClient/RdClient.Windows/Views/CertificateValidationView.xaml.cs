271088a398843ef8a8c3a35a39c5ff77b6c23de0
Alex J <aljugrav@microsoft.com>
2015-06-08 12:30:06 -0700

    handling Enter to press default button in certificate validation dialog

--- RdClient/RdClient.Windows/Views/CertificateValidationView.xaml.cs
+++ RdClient/RdClient.Windows/Views/CertificateValidationView.xaml.cs
@@ -1,9 +1,10 @@
-﻿using RdClient.Shared.Navigation;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-
-namespace RdClient.Views
+﻿namespace RdClient.Views
 {
+    using RdClient.Shared.Helpers;
+    using RdClient.Shared.Navigation;
+    using Windows.System;
+    using Windows.UI.Xaml.Controls;
+
     public sealed partial class CertificateValidationView : Page, IPresentableView
     {
         public CertificateValidationView()
@@ -13,14 +14,33 @@ namespace RdClient.Views
 
         IViewModel IPresentableView.ViewModel
         {
-            get { return this.DataContext as IViewModel; }
+            get { return (IViewModel)this.DataContext; }
         }
 
         void IPresentableView.Activating(object activationParameter) { }
 
-        void IPresentableView.Presenting(INavigationService navigationService, object activationParameter) { }
+        void IPresentableView.Presenting(INavigationService navigationService, object activationParameter)
+        {
+            Dispatcher.AcceleratorKeyActivated += OnAcceleratorKeyActivated;
+        }
 
-        void IPresentableView.Dismissing() { }
+        void IPresentableView.Dismissing()
+        {
+            Dispatcher.AcceleratorKeyActivated -= this.OnAcceleratorKeyActivated;
+        }
 
+        private void OnAcceleratorKeyActivated(Windows.UI.Core.CoreDispatcher sender, Windows.UI.Core.AcceleratorKeyEventArgs e)
+        {
+            switch (e.VirtualKey)
+            {
+                case VirtualKey.Escape:
+                    this.CancelButton.Invoke(e);
+                    break;
+
+                case VirtualKey.Enter:
+                    this.AcceptButton.Invoke(e);
+                    break;
+            }
+        }
     }
 }
