3b21542d230472661469fd20a7704215a33817b5
Pavel Karpenko <pakarpen@microsoft.com>
2015-06-15 11:47:34 -0700

    Add comments and unit test

--- RdClient/RdClient.Shared/Helpers/ViewItemsSource.cs
+++ RdClient/RdClient.Shared/Helpers/ViewItemsSource.cs
@@ -3,10 +3,17 @@
     using System;
     using RdClient.Shared.ViewModels;
 
+    /// <summary>
+    /// Helper implements IViewItemsSource and retains the IItemsView passed to is.
+    /// </summary>
     public sealed class ViewItemsSource : IViewItemsSource
     {
         private IItemsView _itemsView;
 
+        /// <summary>
+        /// Call IItemsView.SelectItem for the view that had set itself by calling IViewItemsSource.SetItemsView.
+        /// </summary>
+        /// <param name="item">View model of the item that must be scrolled into the view and selected.</param>
         public void SelectItem(object item)
         {
             if (null != _itemsView)
