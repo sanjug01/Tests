a7af69890c437883f12ccef3a4f6c766a96174af
unknown <ditodoro@DESKTOP-CUDEF1M.europe.corp.microsoft.com>
2015-07-16 20:49:27 -0700

    figured out which properties to set and how

--- RdClient/RdClient.Shared/CxWrappers/RdpUtils.cs
+++ RdClient/RdClient.Shared/CxWrappers/RdpUtils.cs
@@ -12,7 +12,12 @@ namespace RdClient.Shared.CxWrappers.Utils
         {
             properties.SetStringProperty("Full Address", desktop.HostName);
             properties.SetBoolProperty("Administrative Session", desktop.IsAdminSession);
-            properties.SetIntProperty("AudioMode", (int) desktop.AudioMode);            
+            properties.SetIntProperty("AudioMode", (int) desktop.AudioMode);
+
+            properties.SetIntProperty("DesktopScaleFactor", (int)150);
+            properties.SetIntProperty("DeviceScaleFactor", (int)100);
+
+            properties.SetBoolProperty("High Resolution Mouse", true);
         }
 
         public static void ApplyScreenSize(IRdpProperties properties, IWindowSize windowSize)
