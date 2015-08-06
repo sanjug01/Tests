e275f9bbc7441dfce9541496cdaf7b3ea04d09a1
unknown <ditodoro@DESKTOP-AK3S6KA.europe.corp.microsoft.com>
2015-08-06 13:50:32 -0700

    fixed a crash

--- RdClient/RdClient.Shared/Converters/RdpCertificateToErrorListConverter.cs
+++ RdClient/RdClient.Shared/Converters/RdpCertificateToErrorListConverter.cs
@@ -13,17 +13,15 @@ namespace RdClient.Shared.Converters
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             IRdpCertificate cert = value as IRdpCertificate;
+            IList<string> errorList = new List<string>();
+
             if (TypeToLocalizedStringConverter == null)
             {
                 throw new InvalidOperationException("LocalizedString property must be set before Convert is called");
             }     
-            if (cert == null || cert.Error == null)
-            {
-                throw new ArgumentException("value to convert must be a non-null IRdpCertificate with a non-null Error property");
-            }
-            else
+
+            if(cert != null && cert.Error != null)
             {               
-                IList<string> errorList = new List<string>();
                 foreach(CertificateError err in Enum.GetValues(typeof(CertificateError)))
                 {
                     if(CertificateErrorHelper.ErrorContainsFlag(cert.Error.ErrorFlags, err))
@@ -31,9 +29,9 @@ namespace RdClient.Shared.Converters
                         errorList.Add(this.TypeToLocalizedStringConverter.Convert(err, typeof(string), parameter, language) as string);
                     }
                 }
-
-                return errorList;
             }
+
+            return errorList;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
