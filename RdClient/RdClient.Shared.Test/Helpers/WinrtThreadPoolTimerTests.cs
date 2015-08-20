d50370aff638b7f4aa177c65f8a83de4dd570b3d
James Dudley <james.dudley@microsoft.com>
2015-08-20 11:53:52 -0700

    It appears test framework no longer supports inheriting test methods. They don't run unless they're directly defined in the test class.

--- RdClient/RdClient.Shared.Test/Helpers/WinrtThreadPoolTimerTests.cs
+++ RdClient/RdClient.Shared.Test/Helpers/WinrtThreadPoolTimerTests.cs
@@ -1,12 +1,69 @@
-﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
-using RdClient.Shared.Helpers;
-
-namespace RdClient.Shared.Test.Helpers
+﻿namespace RdClient.Shared.Test.Helpers
 {
+    using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+    using RdClient.Shared.Helpers;
+    using RdClient.Shared.Test.UAP;
+    using System;
+
     [TestClass]
-    public class WinrtThreadPoolTimerTests : ITimerTests
+    public class WinrtThreadPoolTimerTests
     {
-        public override ITimerFactory GetFactory()
+        private ITimerFactory _timerFactory;
+        private ITimer _timer;
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            _timerFactory = GetFactory();
+            _timer = _timerFactory.CreateTimer();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            _timer.Stop();
+        }
+
+        [TestMethod]
+        public void CallStopOnNotRunningTimerDoesntThrow()
+        {
+            _timer.Stop();
+        }
+
+        [TestMethod]
+        public void CallStartOnRunningTimerThrows()
+        {
+            TimeSpan longTimespan = new TimeSpan(1, 0, 0, 0);
+            _timer.Start(() => Assert.Fail("Callback Shouldn't be called"), longTimespan, false);
+            Assert.ThrowsException<InvalidOperationException>(() =>
+            {
+                _timer.Start(() => Assert.Fail("Callback Shouldn't be called"), longTimespan, false);
+            });
+        }
+
+        [TestMethod]
+        public void StartRecurringTimerCallsCallbackMultipleTimes()
+        {
+            int timerCallbacks = 0;
+            TimeSpan shortTimespan = TimeSpan.FromMilliseconds(1.0d);
+            _timer.Start(() => System.Threading.Interlocked.Increment(ref timerCallbacks), shortTimespan, true);
+            UAPSleep.Sleep(50);
+            _timer.Stop();
+            Assert.IsTrue(timerCallbacks > 1);
+        }
+
+        [TestMethod]
+        public void StartNonRecurringTimerCallsCallbackSingleTime()
+        {
+            int timerCallbacks = 0;
+            TimeSpan shortTimespan = TimeSpan.FromMilliseconds(1.0d);
+            _timer.Start(() => System.Threading.Interlocked.Increment(ref timerCallbacks), shortTimespan, false);
+            UAPSleep.Sleep(50);
+            _timer.Stop();
+            Assert.AreEqual(1, timerCallbacks);
+        }
+
+        private ITimerFactory GetFactory()
         {
             return new WinrtThreadPoolTimerFactory();
         }
